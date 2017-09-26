3998f11346458a5363fdfaac683480eb88f59f66
Ivet <SoMLinux>
2017-09-26 14:30:15 +0100

    doubleton inequality tricky, potential mistake

--- HPresolve.h
+++ HPresolve.h
@@ -115,7 +115,7 @@ private:
     void removeColumnSingletons();
     bool removeIfImpliedFree(int col, int i, int k);
     void removeFreeColumnSingleton(const int col, const int row, const int k);
-    void removeColumnSingletonInDoubletonInequality(const int col);
+    void removeColumnSingletonInDoubletonInequality(const int col, const int i);
 
     //dominated columns
     void removeDominatedColumns();
