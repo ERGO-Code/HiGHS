e9c4a9cd5a38346a6b1338fc4030f3b405a7dc00
Julian Hall <jajhall@ed.ac.uk>
2017-10-30 22:48:36 +0000

    After removing ../buildV

--- buildV/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jajhall/h2gmp
-# Build directory: /home/jajhall/h2gmp/buildV
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(src)
-subdirs(check)
