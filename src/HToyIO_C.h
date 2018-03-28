ce7f0f0b188b5599e039c7e0a616c895674e06db
Galabova <bzfgalab@opt50.zib.de>
2018-03-28 16:48:55 +0200

    fixed headers

--- src/HToyIO_C.h
+++ src/HToyIO_C.h
@@ -1,3 +1,8 @@
+#ifndef HTOYIO_C_H
+#define HTOYIO_C_H
+
 int readToy_LP_c(const char *filename, int* m_p, int* n_p, int* maxmin, double* offset,
 		 double ** A,
 		 double ** b, double ** c, double ** lb, double ** ub);
+
+#endif
