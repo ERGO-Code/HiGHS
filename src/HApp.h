53ac94f75b68414ed1943b86085335fa80c7f290
Galabova <bzfgalab@opt63.zib.de>
2018-04-27 17:00:46 +0200

    printHelp created

--- src/HApp.h
+++ src/HApp.h
@@ -56,4 +56,20 @@ int solveExternalPresolve(const char *fileName);
 double presolve(HModel &mod, double &time);
 //int testIO(const char *filename);
 
+void printHelp() {
+  
+        fprintf(stderr, "usage: %s [options] -f fName.mps \n%s", argv[0],
+                "Options: \n"
+                "  -p On(Off): use presolve\n"
+                "  -c mode   : set crash mode to mode. Values:\n"
+                "            : Off LTSSF LTSSF1 LTSSF2 LTSSF3 LTSSF4 LTSSF5 LTSSF6 LTSSF7\n"
+                "  -e edWt   : set edge weight to edWt. Values: \n"
+                "            : Dan Dvx DSE DSE0 DSE1\n\n"
+                "  -s        : use option sip\n"
+                "  -S        : use option SCIP (to test utilities)\n"
+                "  -m [cut]  : use pami. Cutoff optional double value.\n"
+                "  -t fName  : use pami with partition file fName"
+                "  -T time   : use a time limit");
+}
+
 #endif
