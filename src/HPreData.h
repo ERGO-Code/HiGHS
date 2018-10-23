3176a79fed99f4c3f5a40c0e9c99f189e6c99c3a
galabovaa@gmail.com <galabovaa@gmail.com>
2018-10-23 12:12:56 +0100

    comments and formatting, will change HPreData to LPData and load this from parser and remove Checker reference from here.

--- src/HPreData.h
+++ src/HPreData.h
@@ -11,14 +11,16 @@
 
 using namespace std;
 
-struct change {
-		  int type;
-		  int row;
-		  int col;
-		};
+struct change
+{
+	int type;
+	int row;
+	int col;
+};
 
-class HPreData {
-public:
+class HPreData
+{
+ public:
 	HPreData();
 
 	//Model data
@@ -37,9 +39,7 @@ public:
 	vector<double> rowLower;
 	vector<double> rowUpper;
 
-
-
-	//Solution data
+	// Solution data.
 	int numberIteration;
 	double solveTime;
 
@@ -48,54 +48,53 @@ public:
 	vector<double> rowValue;
 	vector<double> rowDual;
 
+	// Row wise copy of matrix.
 	vector<int> ARstart;
 	vector<int> ARindex;
 	vector<double> ARvalue;
 
 	vector<int> Aend;
 
-	//solution
-	vector<double> valuePrimal; //the first numColOriginal elements are the primal variables and slacks after them
+	// Solution
+	// The first numColOriginal elements are the primal variables, slacks after
+	vector<double> valuePrimal;
 	vector<double> valueColDual;
 	vector<double> valueRowDual;
 
-	vector<int> nzCol;		  	//nonzeros in columns and rows
+	vector<int> nzCol; //nonzeros in columns and rows
 	vector<int> nzRow;
 	vector<int> flagCol;
 	vector<int> flagRow;
 
 	vector<int> basicIndex;
-    vector<int> nonbasicFlag;
-    vector<int> nonbasicMove;
+	vector<int> nonbasicFlag;
+	vector<int> nonbasicMove;
 
 	vector<double> colCostAtEl;
 	vector<double> rowLowerAtEl;
 	vector<double> rowUpperAtEl;
 
-
 	void print(int k);
 	void printAR(int i);
 	void makeARCopy();
 	void makeACopy();
-    double getaij(int i, int j);
+	double getaij(int i, int j);
 	bool isZeroA(int i, int j);
- 	void printSolution();
+	void printSolution();
 	double getRowValue(int i);
 
 	stack<double> postValue;
 
-   //to match reduced solution to original
+	//to match reduced solution to original
 	vector<int> rIndex;
 	vector<int> cIndex;
 
-
 	KktChStep chk;
 
-
 	stack<change> chng;
-	stack< pair< int ,vector<double> > > oldBounds; //(j, l, u)
+	stack<pair<int, vector<double>>> oldBounds; //(j, l, u)
 
-	void writeNewFormat(string fileName) ;
+	void writeNewFormat(string fileName);
 };
 
 #endif /* HPREDATA_H_ */
