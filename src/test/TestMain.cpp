7452984c6a1da27e6a22f28f40e6aa4fa129ea42
Ivet GALABOVA <galabovaa@gmail.com>
2018-11-20 11:13:52 +0000

    added a unit test that ctest finds. Linking error currently

--- src/test/TestMain.cpp
+++
@@ -1,8 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "HighsLp.h"
-#include "catch.hpp"
-
-TEST_CASE("status") {
-  std::string str = HighsInputStatusToString(HighsInputStatus::OK);
-  REQUIRE(str == "OK");
-}
