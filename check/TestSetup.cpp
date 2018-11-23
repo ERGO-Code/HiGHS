1b88303a99af58e3c6c773af9b6cd51d00311384
Ivet GALABOVA <galabovaa@gmail.com>
2018-11-23 21:30:47 +0000

    unit testing works

--- check/TestSetup.cpp
+++ check/TestSetup.cpp
@@ -2,7 +2,7 @@
 #include "HighsLp.h"
 
 // No commas in test case name.
-TEST_CASE("correct-print", "[highs_data]") {
+TEST_CASE("correct-print-input-status", "[highs_data]") {
   std::string str = HighsInputStatusToString(HighsInputStatus::OK);
   REQUIRE(str == "OK.");
 }
