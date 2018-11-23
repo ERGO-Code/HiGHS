c723a836ea23ad23ad052e6d03a2e7ca97b4d22c
Ivet GALABOVA <galabovaa@gmail.com>
2018-11-23 16:03:22 +0000

    unable to find test executable

--- check/TestMain.cpp
+++ check/TestMain.cpp
@@ -1,3 +1,9 @@
 #define CATCH_CONFIG_MAIN
-
+#include "HighsLp.h"
 #include "catch.hpp"
+
+// No commas in test case name.
+TEST_CASE("correct-print", "[highs_data]") {
+  std::string str = HighsInputStatusToString(HighsInputStatus::OK);
+  REQUIRE(str == "OK");
+}
