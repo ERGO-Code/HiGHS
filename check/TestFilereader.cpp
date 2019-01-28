bbcb73b81ba4249d816578a693bc1451ddd8c130
Ivet GALABOVA <galabovaa@gmail.com>
2019-01-28 18:22:01 +0000

    test added. test not tested yet.

--- check/TestFilereader.cpp
+++ check/TestFilereader.cpp
@@ -5,6 +5,22 @@
 
 // No commas in test case name.
 TEST_CASE("read-mps-ems", "[highs_filereader]") {
-  std::string str = HighsInputStatusToString(HighsInputStatus::OK);
-  REQUIRE(str == "OK");
+  HighsOptions options;
+  options.filename_ = "/check/instances/adlittle.mps"; // todo: check how to specify path
+  
+  // Read mps.
+  HighsLp lp_mps;
+  HighsInputStatus read_status = loadLpFromFile(options, lp_mps);
+  REQUIRE(read_status == HighsInputStatus::OK);
+
+  // Write ems.
+  FilereaderEms ems;
+  ems.writeModelToFile("filereader-test-ems-file.ems", lp_mps);
+
+  // Read ems and compare.
+  options.filename_ = "filereader-test-ems-file.ems" // todo: check how to specify path
+  HighsLp lp_ems;
+  HighsInputStatus ems_read_status = loadLpFromFile(options, lp_ems);
+
+  REQUIRE(lp_mps == lp_ems); 
 }
