1a6a854dbb5b12d721cc01a02b25065ce8706b28
Ivet GALABOVA <galabovaa@gmail.com>
2019-02-06 17:23:56 +0000

    added test. failing at the moment (nnz = 0)

--- check/TestFilereader.cpp
+++ check/TestFilereader.cpp
@@ -4,6 +4,8 @@
 
 #include "FilereaderEms.h"
 #include "HighsIO.h"
+#include "HMpsFF.h"
+#include "HMPSIO.h"
 #include "HighsLp.h"
 #include "HighsSetup.h"
 #include "LoadProblem.h"
@@ -16,7 +18,35 @@ std::string GetCurrentWorkingDir(void) {
   return current_working_dir;
 }
 
-// No commas in test case name.
+TEST_CASE("free-format-parser", "[highs_filereader]") {
+  std::string dir = GetCurrentWorkingDir();
+
+  std::cout << dir << std::endl;
+
+  // For debugging use the latter.
+  // std::string filename = dir + "/../../check/instances/25fv47.mps";
+  std::string filename = dir + "/check/instances/adlittle.mps";
+
+  // Read mps.
+  HighsLp lp_free_format, lp_fixed_format;
+
+  HMpsFF parser{};
+  int status = parser.loadProblem(filename, lp_free_format);
+
+  std::vector<int> integerColumn;
+  status = readMPS(filename.c_str(), -1, -1, lp_fixed_format.numRow_,
+                   lp_fixed_format.numCol_, lp_fixed_format.sense_,
+                   lp_fixed_format.offset_, lp_fixed_format.Astart_,
+                   lp_fixed_format.Aindex_, lp_fixed_format.Avalue_,
+                   lp_fixed_format.colCost_, lp_fixed_format.colLower_,
+                   lp_fixed_format.colUpper_, lp_fixed_format.rowLower_,
+                   lp_fixed_format.rowUpper_, integerColumn,
+                   lp_fixed_format.row_names_, lp_fixed_format.col_names_);
+
+  bool are_the_same = lp_free_format == lp_fixed_format;
+  REQUIRE(are_the_same);
+}
+
 TEST_CASE("read-mps-ems", "[highs_filereader]") {
   HighsOptions options;
   std::string dir = GetCurrentWorkingDir();
