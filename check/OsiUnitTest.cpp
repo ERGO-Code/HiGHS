5c7cedc0fdb768c6e99a6fe08e9cb698eacf07da
Stefan Vigerske <svigerske@gams.com>
2019-02-18 22:55:54 +0100

    add file header comment

--- check/OsiUnitTest.cpp
+++ check/OsiUnitTest.cpp
@@ -1,4 +1,16 @@
-// TODO license
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                       */
+/*    This file is part of the HiGHS linear optimization suite           */
+/*                                                                       */
+/*    Written and engineered 2008-2019 at the University of Edinburgh    */
+/*                                                                       */
+/*    Available as open-source under the MIT License                     */
+/*                                                                       */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/**@file OsiUnitTest
+ * @brief Osi/HiGHS unit test
+ * @author Julian Hall, Ivet Galabova, Qi Huangfu and Michael Feldmeier
+ */
 
 #include "CoinPragma.hpp"
 
