94f1a6cfa17f15bd20b19176a2faaaff7f62b4c4
Julian Hall <jajhall@ed.ac.uk>
2019-02-24 16:53:42 +0000

    Adding check/TestLpModification.cpp unit test

---
+++ check/TestLpModification.cpp
@@ -0,0 +1,35 @@
+#include "catch.hpp"
+#include "Highs.h"
+#include "Avgas.h"
+
+// No commas in test case name.
+TEST_CASE("LP-modification", "[highs_data]") {
+  // Create an empty LP
+  HighsLp lp;
+  HighsOptions options;
+  HighsSetMessagelevel(ML_ALWAYS);
+
+  Avgas avgas;
+  int num_row;
+  int num_nz;
+  vector<double> rowLower;
+  vector<double> rowUpper;
+  avgas.rows(num_row, rowLower, rowUpper);
+
+  int num_col = 0;
+  vector<double> colCost;
+  vector<double> colLower;
+  vector<double> colUpper;
+  vector<int> Astart;
+  vector<int> Aindex;
+  vector<double> Avalue;
+  for (int col = 0; col < 8; col++) {
+    avgas.col(col, num_col, num_nz, colCost, colLower, colUpper, Astart, Aindex, Avalue);
+  }
+  Highs highs(options);
+  HighsStatus return_status;
+  return_status = highs.initializeLp(lp);
+  //  printf("initializeLp: return_status = %s\n", HighsStatusToString(return_status).c_str());
+  REQUIRE(return_status == HighsStatus::OK);
+}
+
