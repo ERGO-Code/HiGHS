90da787f2eb168e7c9bf855912412ee18f6a2271
Michael Feldmeier <s1613957@ed.ac.uk>
2019-02-27 16:37:49 +0000

    added python interface and test code

---
+++ src/interfaces/highs_lp_solver.py
@@ -0,0 +1,18 @@
+import ctypes
+highs = ctypes.cdll.LoadLibrary("../build/lib/libhighs.so") # change path
+
+highs.callhighs.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_double))
+
+def call_highs(colcost, collower, colupper, rowlower, rowupper, astart, aindex, avalue):
+   global highs
+   n_col = len(colcost)
+   n_row = len(rowlower)
+   n_nz = len(aindex)
+
+   dbl_array_type_col = ctypes.c_double * n_col
+   dbl_array_type_row = ctypes.c_double * n_row
+   int_array_type_astart = ctypes.c_int * (n_col + 1)
+   int_array_type_aindex = ctypes.c_int * n_nz
+   dbl_array_type_avalue = ctypes.c_double * n_nz
+
+   highs.callhighs(ctypes.c_int(n_col), ctypes.c_int(n_row), ctypes.c_int(n_nz), dbl_array_type_col(*colcost), dbl_array_type_col(*collower), dbl_array_type_col(*colupper), dbl_array_type_row(*rowlower), dbl_array_type_row(*rowupper), int_array_type_astart(*astart), int_array_type_aindex(*aindex), dbl_array_type_avalue(*avalue))
