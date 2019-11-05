82104760c9627a4aaa0c73ad26e1e331519ce1ef
Ivet <galabovaa@gmail.com>
2019-11-05 17:48:23 +0000

    getting ipx error cosntructing starting basis

--- src/interior_point/IpxWrapper.h
+++ src/interior_point/IpxWrapper.h
@@ -1,6 +1,8 @@
 #ifndef INTERIOR_POINT_IPX_WRAPPER_H_
 #define INTERIOR_POINT_IPX_WRAPPER_H_
 
+#include <algorithm>
+
 #include "interior_point/ipx/include/ipx_status.h"
 #include "interior_point/ipx/src/lp_solver.h"
 #include "lp_data/HConst.h"
@@ -26,22 +28,28 @@ IpxStatus fillInIpxData(const HighsLp& lp, ipx::Int& num_col,
   // transfer bounds.
   assert(lp.rowLower_.size() == (unsigned int)num_row);
   assert(lp.rowUpper_.size() == (unsigned int)num_row);
+
   std::vector<int> general_bounded_rows;
+  std::vector<int> free_rows;
 
   for (int row = 0; row < num_row; row++)
-    if (lp.rowLower_[row] > -HIGHS_CONST_INF &&
+    if (lp.rowLower_[row] < lp.rowUpper_[row] &&
+        lp.rowLower_[row] > -HIGHS_CONST_INF &&
         lp.rowUpper_[row] < HIGHS_CONST_INF)
       general_bounded_rows.push_back(row);
+    else if (lp.rowLower_[row] == -HIGHS_CONST_INF &&
+             lp.rowUpper_[row] == HIGHS_CONST_INF)
+      free_rows.push_back(row);
 
   const int num_slack = general_bounded_rows.size();
 
-  // For each row (assuming no free rows) add entry to char array and set up rhs
+  // For each row except free rows add entry to char array and set up rhs
   // vector
   rhs.reserve(num_row);
   constraint_type.reserve(num_row);
 
   for (int row = 0; row < num_row; row++) {
-    if (lp.rowLower_[row] > HIGHS_CONST_INF &&
+    if (lp.rowLower_[row] > -HIGHS_CONST_INF &&
         lp.rowUpper_[row] == HIGHS_CONST_INF) {
       rhs.push_back(lp.rowLower_[row]);
       constraint_type.push_back('>');
@@ -52,29 +60,67 @@ IpxStatus fillInIpxData(const HighsLp& lp, ipx::Int& num_col,
     } else if (lp.rowLower_[row] == lp.rowUpper_[row]) {
       rhs.push_back(lp.rowUpper_[row]);
       constraint_type.push_back('=');
-    } // else: Free row. Ignored.
+    } else if (lp.rowLower_[row] > -HIGHS_CONST_INF &&
+               lp.rowUpper_[row] < HIGHS_CONST_INF) {
+      // general bounded
+      rhs.push_back(0);
+      constraint_type.push_back('=');
+    }
   }
 
+  std::vector<int> reduced_rowmap(lp.numRow_, -1);
+  if (free_rows.size() > 0) {
+    int counter = 0;
+    int findex = 0;
+    for (int row = 0; row < lp.numRow_; row++) {
+      if (free_rows[findex] == row) {
+        findex++;
+        continue;
+      } else {
+        reduced_rowmap[row] = counter;
+        counter++;
+      }
+    }
+  } else {
+    for (int k = 0; k < lp.numRow_; k++) reduced_rowmap[k] = k;
+  }
+  num_row -= free_rows.size();
   num_col += num_slack;
 
+  std::vector<int> sizes(num_col, 0);
+
+  for (int col = 0; col < lp.numCol_; col++)
+    for (int k = lp.Astart_[col]; k < lp.Astart_[col + 1]; k++) {
+      int row = lp.Aindex_[k];
+      if (lp.rowLower_[row] > -HIGHS_CONST_INF ||
+          lp.rowUpper_[row] < HIGHS_CONST_INF)
+        sizes[col]++;
+    }
   // Copy Astart and Aindex to ipx::Int array.
   int nnz = lp.Aindex_.size();
   Ap.resize(num_col + 1);
-  Ai.resize(nnz + num_slack);
-  Ax.resize(nnz + num_slack);
+  Ai.reserve(nnz + num_slack);
+  Ax.reserve(nnz + num_slack);
 
   // Set starting points of original and newly introduced columns.
-  for (int col = 0; col <= lp.numCol_; col++) Ap[col] = lp.Astart_[col];
+  for (int col = 1; col <= lp.numCol_; col++)
+    Ap[col] = lp.Astart_[col - 1] + sizes[col - 1];
   for (int col = lp.numCol_; col <= (int)num_col; col++) {
     Ap[col] = Ap[col - 1] + 1;
   }
+
   for (int k = 0; k < nnz; k++) {
-    Ai[k] = lp.Aindex_[k];
-    Ax[k] = lp.Avalue_[k];
+    int row = lp.Aindex_[k];
+    if (lp.rowLower_[row] > -HIGHS_CONST_INF ||
+        lp.rowUpper_[row] < HIGHS_CONST_INF) {
+      Ai.push_back(reduced_rowmap[lp.Aindex_[k]]);
+      Ax.push_back(lp.Avalue_[k]);
+    }
   }
+
   for (int k = 0; k < num_slack; k++) {
-    Ai[nnz + k] = (ipx::Int)general_bounded_rows[k];
-    Ax[nnz + k] = -1;
+    Ai.push_back((ipx::Int)general_bounded_rows[k]);
+    Ax.push_back(-1);
   }
 
   // Column bound vectors.
@@ -127,7 +173,7 @@ IpxStatus solveModelWithIpx(const HighsLp& lp, HighsSolution& solution) {
       lps.Solve(num_col, &objective[0], &col_lb[0], &col_ub[0], num_row, &Ap[0],
                 &Ai[0], &Av[0], &rhs[0], &constraint_type[0]);
 
-  if (status != IPX_STATUS_optimal) {
+  if (status != IPX_STATUS_solved) {
     // fatal error (invalid input, out of memory, etc.)
     std::cout << " status: " << status << ','
               << " errflag: " << lps.GetInfo().errflag << '\n';
