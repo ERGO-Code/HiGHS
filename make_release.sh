b5ab9092fef71c6ee6ab618f4c219aff716d2dd7
mckib2 <nicholas.bgp@gmail.com>
2020-03-30 21:35:32 -0600

    Remove wheel generation while I get manylinux sorted

--- make_release.sh
+++ make_release.sh
@@ -10,7 +10,7 @@ git log --pretty=format:'%h' -n 1  > GITHASH
 # Generate distribution archives (both source and binary)
 pip install --upgrade setuptools wheel
 python setup.py sdist
-python setup.py bdist_wheel
+#python setup.py bdist_wheel
 
 # Upload
 pip install --upgrade twine
