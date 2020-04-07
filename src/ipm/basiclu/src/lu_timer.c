59c31a2e87bfd4c8bf68e340d65560642a4f1b5b
Ivet Galabova <galabovaa@gmail.com>
2020-04-07 12:31:14 +0100

    malloc removed and added macro def check in lu_timer

--- src/ipm/basiclu/src/lu_timer.c
+++ src/ipm/basiclu/src/lu_timer.c
@@ -15,23 +15,23 @@
 
 #else
 
-#define _POSIX_C_SOURCE 199309L
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L
+#endif
 
 #endif
 
 #include "lu_timer.h"
 
-void lu_tic (double tic[2])
-{
-    struct timespec t;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t);
-    tic[0] = (double) t.tv_sec;
-    tic[1] = (double) t.tv_nsec;
+void lu_tic(double tic[2]) {
+  struct timespec t;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &t);
+  tic[0] = (double)t.tv_sec;
+  tic[1] = (double)t.tv_nsec;
 }
 
-double lu_toc (const double tic[2])
-{
-    double toc[2];
-    lu_tic(toc);
-    return (toc[0] - tic[0]) + 1e-9*(toc[1] - tic[1]);
+double lu_toc(const double tic[2]) {
+  double toc[2];
+  lu_tic(toc);
+  return (toc[0] - tic[0]) + 1e-9 * (toc[1] - tic[1]);
 }
