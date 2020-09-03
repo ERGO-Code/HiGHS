eca5af1fbbda39f9e9685f7b5cd6115939299310
Julian Hall <jajhall@ed.ac.uk>
2020-09-03 18:34:39 +0100

    Introduced debugSimplexDualFeasibility to HSimplexDebug

--- src/simplex/HSimplexDebug.h
+++ src/simplex/HSimplexDebug.h
@@ -44,6 +44,10 @@ HighsDebugStatus debugComputeDual(const HighsModelObject& highs_model_object,
                                   const std::vector<double>& basic_costs,
                                   const std::vector<double>& row_dual);
 
+HighsDebugStatus debugSimplexDualFeasibility(const HighsModelObject& highs_model_object,
+					     const std::string message,
+					     const bool force = false); 
+
 HighsDebugStatus debugUpdatedObjectiveValue(
     HighsModelObject& highs_model_object, const SimplexAlgorithm algorithm,
     const int phase, const std::string message, const bool force = false);
