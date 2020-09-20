69066eb02d627c930eeff53f20dc46bf40ae6f49
galabovaa <galabovaa@gmail.com>
2020-09-20 17:22:34 +0100

    fast build experimental mode added

--- check/doctest/TestPresolveColumnSingletons.cpp
+++ check/doctest/TestPresolveColumnSingletons.cpp
@@ -1,5 +1,15 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
 #include "Highs.h"
-#include "catch.hpp"
+
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("testing the factorial function") {
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(2) == 2);
+    CHECK(factorial(3) == 6);
+    CHECK(factorial(10) == 3628800);
+}
 
 // New tests, for each of the special cases.
 // - col sing doubleton equality 
@@ -227,35 +237,35 @@ HighsStatus twoColSingDoubletonInequality()
 }
 
 // No commas in test case name.
-TEST_CASE("zero-cost", "[presolve-col-sing]") {
+TEST_CASE("zero-cost [presolve-col-sing]") {
   std::cout << "Presolve 1." << std::endl;
   HighsStatus status = zeroCostColSing();
   std::string str = HighsStatusToString(status);
-  REQUIRE(str == "OK");
+  CHECK(str == "OK");
 }
 
-TEST_CASE("col-sing-doubleton-eq", "[presolve-col-sing]") {
+TEST_CASE("col-sing-doubleton-eq [presolve-col-sing]") {
   std::cout << "Presolve 2." << std::endl;
   HighsStatus status =  colSingDoubletonEquality();
   std::string str = HighsStatusToString(status);
-  REQUIRE(str == "OK");
+  CHECK(str == "OK");
 }
 
-TEST_CASE("col-sing-doubleton-ineq", "[presolve-col-sing]") {
+TEST_CASE("col-sing-doubleton-ineq [presolve-col-sing]") {
   std::cout << "Presolve 3." << std::endl;
   HighsStatus status =  colSingDoubletonInequality();
   std::string str = HighsStatusToString(status);
-  REQUIRE(str == "OK");
+  CHECK(str == "OK");
 }
 
-TEST_CASE("two-col-sing-doubleton-eq", "[presolve-col-sing]") {
+TEST_CASE("two-col-sing-doubleton-eq [presolve-col-sing]") {
   std::cout << "Presolve 4." << std::endl;
   HighsStatus status =  twoColSingDoubletonEquality();
   std::string str = HighsStatusToString(status);
-  REQUIRE(str == "OK");
+  CHECK(str == "OK");
 }
 
-TEST_CASE("two-col-sing-doubleton-ineq", "[presolve-col-sing]") {
+TEST_CASE("two-col-sing-doubleton-ineq [presolve-col-sing]") {
   std::cout << "Presolve 5." << std::endl;
   HighsStatus status =  twoColSingDoubletonInequality();
   std::string str = HighsStatusToString(status);
