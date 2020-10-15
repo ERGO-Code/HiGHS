d13d07510bda17328ee220b9b35c0a371e862185
Julian Hall <jajhall@ed.ac.uk>
2020-10-15 21:38:18 +0100

    getHighsRanging now passes back ranging as a parameter and returns HighsStatus

--- src/lp_data/HighsRanging.cpp
+++ src/lp_data/HighsRanging.cpp
@@ -18,5 +18,24 @@
 
 HighsStatus getHighsRanging(HighsRanging& ranging,
                             const HighsModelObject& highs_model_object) {
+  if (highs_model_object.scaled_model_status_ != HighsModelStatus::OPTIMAL) {
+    highsLogMessage(highs_model_object.options_.logfile, HighsMessageType::ERROR,
+		    "Cannot get ranging without an optimal solution");
+    return HighsStatus::Error;
+  }
+  if (!highs_model_object.simplex_lp_status_.valid) {
+    highsLogMessage(highs_model_object.options_.logfile, HighsMessageType::ERROR,
+		    "Cannot get ranging without a valid Simplex LP");
+    return HighsStatus::Error;
+  }
+  const HighsSimplexInfo& simplex_info = highs_model_object.simplex_info_;
+  const HighsSimplexBasis& simplex_basis = highs_model_object.simplex_basis_;
+  const vector<int>& Bvalue_ = simplex_basis.BasicIndex_;
+  vector<double> xi = Bvalue_;
+  for (int i = 0; i < numRow; i++) {
+    xi[i] = max(xi[i], Blower_[i]);
+    xi[i] = min(xi[i], Bupper_[i]);
+  }
+
   return HighsStatus::OK;
 }
