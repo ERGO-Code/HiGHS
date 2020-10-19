15e6445504f64a0107a7423789a457751c256ea2
Julian Hall <jajhall@ed.ac.uk>
2020-10-19 14:46:25 +0100

    Removed commented out HighsStruct.h from Presolve.h

--- src/presolve/Presolve.h
+++ src/presolve/Presolve.h
@@ -23,7 +23,6 @@
 #include <vector>
 
 #include "lp_data/HighsLp.h"
-//#include "lp_data/HStruct.h"
 #include "lp_data/HighsSolution.h"
 #include "presolve/HPreData.h"
 #include "presolve/PresolveAnalysis.h"
