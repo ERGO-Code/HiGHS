26690c5dfb0eb7446669b9f8777da776934a5989
Julian Hall <jajhall@ed.ac.uk>
2021-02-16 15:06:51 +0000

    Returned min_highs_debug_level to default

--- src/lp_data/Highs.cpp
+++ src/lp_data/Highs.cpp
@@ -366,12 +366,17 @@ HighsStatus Highs::writeBasis(const std::string filename) {
 // Checks the options calls presolve and postsolve if needed. Solvers are called
 // with callSolveLp(..)
 HighsStatus Highs::run() {
-  const int min_highs_debug_level =  // HIGHS_DEBUG_LEVEL_MIN;
-                                     //   HIGHS_DEBUG_LEVEL_CHEAP;
-      HIGHS_DEBUG_LEVEL_COSTLY;
-  //      HIGHS_DEBUG_LEVEL_EXPENSIVE;
+  const int min_highs_debug_level = HIGHS_DEBUG_LEVEL_MIN;
+  //  HIGHS_DEBUG_LEVEL_CHEAP;
+  //  HIGHS_DEBUG_LEVEL_COSTLY;
+  //  HIGHS_DEBUG_LEVEL_EXPENSIVE;
   //  HIGHS_DEBUG_LEVEL_MAX;
 #ifdef HiGHSDEV
+  min_highs_debug_level =  // HIGHS_DEBUG_LEVEL_MIN;
+                           //  HIGHS_DEBUG_LEVEL_CHEAP;
+      HIGHS_DEBUG_LEVEL_COSTLY;
+  //  HIGHS_DEBUG_LEVEL_EXPENSIVE;
+  //  HIGHS_DEBUG_LEVEL_MAX;
   if (options_.highs_debug_level < min_highs_debug_level)
     printf(
         "Highs::run() HiGHSDEV define so switching options_.highs_debug_level "
