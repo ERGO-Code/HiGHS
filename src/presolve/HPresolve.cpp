f0064b96ca25e5279c6ead8628a3692774d2d26c
HALL Julian <jajhall@ed.ac.uk>
2021-03-18 09:34:26 +0000

    Added #include <algorithm> to HighsLinearSumBounds.cpp (and 2xIPX files) plus code for segfault in HPresolve.cpp

--- src/presolve/HPresolve.cpp
+++ src/presolve/HPresolve.cpp
@@ -4361,7 +4361,12 @@ HPresolve::Result HPresolve::detectParallelRowsAndCols(
       HPRESOLVE_CHECKED_CALL(checkLimits(postSolveStack));
       HPRESOLVE_CHECKED_CALL(removeRowSingletons(postSolveStack));
     } else
+      printf("In HPresolve::detectParallelRowsAndCols: before buckets.emplace_hint(%d, %d, %d)\n", last, colHashes[i], i);fflush(stdout);
+    for(int jh = 0; jh < model->numCol_; jh++) {
+      printf("In HPresolve::detectParallelRowsAndCols: colHashes[%d] = %d\n", jh,  colHashes[jh]);fflush(stdout);
+    }
       buckets.emplace_hint(last, colHashes[i], i);
+    printf("In HPresolve::detectParallelRowsAndCols: after buckets.emplace_hint(last, colHashes[i], i)\n");fflush(stdout);
   }
 
   buckets.clear();
