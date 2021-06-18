fddb7a53e807761dd1ff5c496848ea59ea83306d
Leona Gottwald <leona.gottwald@posteo.de>
2021-06-18 18:59:35 +0200

    change sparsify logging to highsLogDev

--- src/presolve/HPresolve.cpp
+++ src/presolve/HPresolve.cpp
@@ -3057,8 +3057,8 @@ HPresolve::Result HPresolve::presolve(HighsPostsolveStack& postSolveStack) {
         HPRESOLVE_CHECKED_CALL(sparsify(postSolveStack));
         double nzReduction = 100.0 * (1.0 - (numNonzeros() / (double)numNz));
 
-        if (nzReduction >= 0.95) {
-          highsLogUser(options->log_options, HighsLogType::kInfo,
+        if (nzReduction > 0) {
+          highsLogDev(options->log_options, HighsLogType::kInfo,
                        "Sparsify removed %.1f%% of nonzeros\n", nzReduction);
 
           fastPresolveLoop(postSolveStack);
