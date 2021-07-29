1892c9b5de5632ced30ee08cef19154c0dbb42ad
Leona Gottwald <leona.gottwald@posteo.de>
2021-07-29 10:36:05 +0200

    fix missing virtual dtor

--- src/qpsolver/pricing.hpp
+++ src/qpsolver/pricing.hpp
@@ -9,6 +9,7 @@ class Pricing {
   virtual HighsInt price(const Vector& x, const Vector& gradient) = 0;
   virtual void update_weights(const Vector& aq, const Vector& ep, HighsInt p,
                               HighsInt q) = 0;
+  virtual ~Pricing() {}
 };
 
 #endif
