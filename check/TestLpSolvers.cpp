64ec9dacc60b20a2148257a2d949f3ea0ec6edac
Julian Hall <jajhall@ed.ac.uk>
2021-09-01 18:46:49 +0100

    Another iteration number changed!

--- check/TestLpSolvers.cpp
+++ check/TestLpSolvers.cpp
@@ -301,7 +301,7 @@ TEST_CASE("LP-solver", "[highs_lp_solver]") {
   return_status = highs.run();
   REQUIRE(return_status == HighsStatus::kOk);
 
-  REQUIRE(info.simplex_iteration_count == 690);
+  REQUIRE(info.simplex_iteration_count == 619);
 }
 
 TEST_CASE("dual-objective-upper-bound", "[highs_lp_solver]") {
