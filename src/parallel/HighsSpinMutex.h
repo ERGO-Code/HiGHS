95e6bb26997cd5bac78c32a1aedaa2d48ca3fe99
Leona Gottwald <leona.gottwald@posteo.de>
2021-10-30 13:50:46 +0200

    finalize spin mutex implementation

--- src/parallel/HighsSpinMutex.h
+++ src/parallel/HighsSpinMutex.h
@@ -21,19 +21,18 @@
 #ifdef HIGHS_MM_PAUSE_REQUIRES_IMMINTRIN
 #include "immintrin.h"
 #elif !defined(HIGHS_HAVE_MM_PAUSE)
-#inlude <thread>
+#include <thread>
 #endif
 
-
 class HighsSpinMutex {
-  std::atomic_bool flag;
+  std::atomic_bool flag{true};
 
  public:
   static void yieldProcessor() {
 #ifdef HIGHS_HAVE_MM_PAUSE
     _mm_pause();
 #else
-  std::this_thread::yield();
+    std::this_thread::yield();
 #endif
   }
 
@@ -41,13 +40,11 @@ class HighsSpinMutex {
     while (true) {
       if (!flag.exchange(true, std::memory_order_acquire)) return;
 
-      while (flag.load(std::memory_order_relaxed)) {
-        yieldProcessor();
-      }
+      while (flag.load(std::memory_order_relaxed)) yieldProcessor();
     }
   }
 
-  void unlock() noexcept {}
+  void unlock() noexcept { flag.store(false, std::memory_order_release); }
 };
 
 #endif
