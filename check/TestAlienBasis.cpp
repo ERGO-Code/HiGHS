d79b854eb14c8c555fe4659da8da94d070918a3c
Julian Hall <jajhall@ed.ac.uk>
2021-11-08 23:07:54 +0000

    Transform use of numRow numCol in HFactor to num_row and num_col

--- check/TestAlienBasis.cpp
+++ check/TestAlienBasis.cpp
@@ -2,7 +2,7 @@
 #include "catch.hpp"
 
 const double inf = kHighsInf;
-const bool dev_run = false;
+const bool dev_run = true;
 const double double_equal_tolerance = 1e-5;
 
 TEST_CASE("AlienBasis-avgas", "[highs_test_alien_basis]") {
@@ -19,6 +19,7 @@ TEST_CASE("AlienBasis-avgas", "[highs_test_alien_basis]") {
   basis.col_status.resize(num_col);
   basis.row_status.resize(num_row);
   basis.debug_origin_name = "AlienBasis-avgas";
+  // Create a full-dimension basis using struturals and then enough logicals
   HighsBasisStatus status = HighsBasisStatus::kBasic;
   for (HighsInt iCol = 0; iCol < num_col; iCol++) {
     if (iCol >= num_row) status = HighsBasisStatus::kNonbasic;
@@ -28,6 +29,14 @@ TEST_CASE("AlienBasis-avgas", "[highs_test_alien_basis]") {
     if (num_col + iRow >= num_row) status = HighsBasisStatus::kNonbasic;
     basis.row_status[iRow] = status;
   }
+  REQUIRE(highs.setBasis(basis) == HighsStatus::kOk);
+  highs.run();
+  // Create a rectangular basis using just struturals
+  for (HighsInt iCol = 0; iCol < num_col; iCol++)
+    basis.col_status[iCol] = HighsBasisStatus::kBasic;
+  assert(num_col < num_row);
+  for (HighsInt iRow = 0; iRow < num_row; iRow++)
+    basis.row_status[iRow] = HighsBasisStatus::kNonbasic;
   REQUIRE(highs.setBasis(basis) == HighsStatus::kOk);
   highs.run();
 }
