d33e8edc7524d872b0db991a043603a74f0e206a
Michael Bynum <mlbynum@sandia.gov>
2022-02-23 07:41:43 -0700

    adding log callback to highspy interface

--- highspy/__init__.py
+++ highspy/__init__.py
@@ -1,1 +1,13 @@
-from .highs_bindings import *
+from .highs import (
+    ObjSense,
+    HighsModelStatus,
+    HighsVarType,
+    HighsStatus,
+    HighsLogType,
+    HighsSolution,
+    Highs,
+    kHighsInf,
+    HIGHS_VERSION_MAJOR,
+    HIGHS_VERSION_MINOR,
+    HIGHS_VERSION_PATCH,
+)
