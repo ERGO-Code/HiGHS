3c1bba20603bb5dcd969c1b616137ad603a2d2c3
Julian Hall <jajhall@ed.ac.uk>
2022-05-05 20:04:43 +0100

    Removed MPS example from examples/call_highs_from_python.py

--- examples/call_highs_from_python.py
+++ examples/call_highs_from_python.py
@@ -71,18 +71,3 @@ return_status, model_status, col_value, col_dual, row_value, row_dual , col_basi
 print ("return_status = ", return_status)
 print ("model_status = ", model_status)
 print (col_value, col_dual, row_value, row_dual, col_basis, row_basis)
-
-# Illustrate the reading of an MPS file
-#
-return_status, n_col, n_row, sense, offset, col_cost, col_lower, col_upper, row_lower, row_upper, a_start, a_index, a_value = Highs_lpMpsRead()
-
-# Find the continuous solution of the LP
-return_status, model_status, col_value, col_dual, row_value, row_dual , col_basis, row_basis = Highs_lpCall(
-    col_cost, col_lower, col_upper,
-    row_lower, row_upper,
-    a_start, a_index, a_value)
-
-print ("return_status = ", return_status)
-print ("model_status = ", model_status)
-print (col_value)
-
