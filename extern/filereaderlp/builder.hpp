10101ad997117b63a4c854c409f9d645324cfc0b
Stefan Vigerske <svigerske@gams.com>
2022-07-19 15:44:41 +0200

    speed up getvarbyname() a bit
    - no need to copy argument
    - lookup in variables array only once

--- extern/filereaderlp/builder.hpp
+++ extern/filereaderlp/builder.hpp
@@ -12,12 +12,14 @@ struct Builder {
 
    Model model;
 
-   std::shared_ptr<Variable> getvarbyname(std::string name) {
-      if (variables.count(name) == 0) {
-         variables[name] = std::shared_ptr<Variable>(new Variable(name));
-         model.variables.push_back(variables[name]);
-      }
-      return variables[name];
+   std::shared_ptr<Variable> getvarbyname(const std::string& name) {
+      auto it = variables.find(name);
+      if (it != variables.end())
+         return it->second;
+      auto newvar = std::shared_ptr<Variable>(new Variable(name));
+      variables.insert(std::make_pair(name, newvar));
+      model.variables.push_back(newvar);
+      return newvar;
    }
 };
 
