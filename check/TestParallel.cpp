7db79319f0666dc2283a517581776fe599895310
Julian Hall <jajhall@ed.ac.uk>
2022-08-11 15:01:08 +0100

    Before adding Highs::run() calls

--- check/TestParallel.cpp
+++ check/TestParallel.cpp
@@ -5,37 +5,49 @@ using namespace highs;
 
 const HighsInt numThreads = (std::thread::hardware_concurrency() + 1) / 2;
 const bool dev_run = true;
-const HighsInt dumb_work_n = 10;
 
-void dumb_work();
-double dumb_work(const HighsInt n);
+void dumbWork();
+double dumbWork(const HighsInt n);
 
-TEST_CASE("test-parallel-spawn", "[highs_test_parallel]") {
-  if (dev_run)
+void noReturnSpawn();
+void returnSpawn();
+
+TEST_CASE("test-parallel", "[highs_test_parallel]") {
+  if (dev_run) {
     printf("std::thread::hardware_concurrency() returns %d\n",
            std::thread::hardware_concurrency());
-  if (dev_run) printf("Using %d threads\n", int(numThreads));
+    printf("Using %d threads\n", int(numThreads));
+  }
   HighsTaskExecutor::shutdown();
   parallel::initialize_scheduler(numThreads);
-  // Why is the following "invalid use of void expression"
-  //  parallel::spawn(dumb_work());
-  //  dumb_work();
-  //  parallel::sync();
-  double v10;
-  parallel::spawn([&]() { v10 = dumb_work(10); });
-  const double v11 = dumb_work(11);
-  printf("Before sync(): v10 = %g; v11 = %g\n", v10, v11);
-  parallel::sync();
-  printf("After  sync(): v10 = %g; v11 = %g\n", v10, v11);
+  noReturnSpawn();
+  returnSpawn();
 }
 
-void dumb_work() {
+void noReturnSpawn() {
+  // Why is the following "invalid use of void expression"
+  //  parallel::spawn(dumbWork());
+  //  dumbWork();
+  //  parallel::sync();
+}
+
+void returnSpawn() {
+  double v10;
+  parallel::spawn([&]() { v10 = dumbWork(10); });
+  const double v11 = dumbWork(11);
+  if (dev_run) printf("Before sync(): v10 = %g; v11 = %g\n", v10, v11);
+  parallel::sync();
+  if (dev_run) printf("After  sync(): v10 = %g; v11 = %g\n", v10, v11);
+}
+
+void dumbWork() {
+  const HighsInt dumb_work_n = 10;
   double sum_products = 0;
   for (HighsInt i = 1; i <= dumb_work_n; i++)
     for (HighsInt j = 1; i <= dumb_work_n; i++) sum_products += i * j;
 }
 
-double dumb_work(const HighsInt n) {
+double dumbWork(const HighsInt n) {
   double sum_products = 0;
   for (HighsInt i = 1; i <= n; i++)
     for (HighsInt j = 1; i <= n; i++) sum_products += i * j;
