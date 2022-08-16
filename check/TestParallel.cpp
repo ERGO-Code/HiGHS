453942deaa94c74c470f5fed9d1852dd852777e9
Julian Hall <jajhall@ed.ac.uk>
2022-08-16 11:00:43 +0100

    Cleaned up TestParallel to use just vector of pointers to Highs instances

--- check/TestParallel.cpp
+++ check/TestParallel.cpp
@@ -108,47 +108,33 @@ double concurrentLpSolve(const bool use_race_timer) {
   lp_solvers.push_back(kLpSolverDualSimplex);
   lp_solvers.push_back(kLpSolverInteriorPoint);
   HighsInt num_lp_solvers = lp_solvers.size();
-  // @Leona this (in /*...*/) should work:
-  //
-  // @Leona this doesn't compile: it gives
-  // @Julian, ah I see, then use the unique_ptr version. This requires the HiGHS class to get a move constructor and/or a copy constructor. Wrapping it in a unique_ptr works around that.
-  // adding a move/copy constructor should not be too hard, but there are some members, e.g. the FILE* for input/output handling that might need special handling, so the unique_ptr version is simpler than adding a move contructor.
-  // In file included from /usr/include/c++/9/vector:66,
-  //                  from /home/jajhall/HiGHS/src/lp_data/HighsLpUtils.h:19,
-  //                  from /home/jajhall/HiGHS/src/Highs.h:21,
-  //                  from /home/jajhall/HiGHS/check/TestParallel.cpp:1:
-// /usr/include/c++/9/bits/stl_uninitialized.h: In instantiation of ‘_ForwardIterator std::uninitialized_copy(_InputIterator, _In// putIterator, _ForwardIterator) [with _InputIterator = std::move_iterator<Highs*>; _ForwardIterator = Highs*]’:
-// /usr/include/c++/9/bits/stl_uninitialized.h:307:37:   required from ‘_ForwardIterator std::__uninitialized_copy_a(_InputIterator, _InputIterator, _ForwardIterator, std::allocator<_Tp>&) [with _InputIterator = std::move_iterator<Highs*>; _ForwardIterator = Highs*; _Tp = Highs]’
-// /usr/include/c++/9/bits/stl_uninitialized.h:329:2:   required from ‘_ForwardIterator std::__uninitialized_move_if_noexcept_a(_InputIterator, _InputIterator, _ForwardIterator, _Allocator&) [with _InputIterator = Highs*; _ForwardIterator = Highs*; _Allocator = std::allocator<Highs>]’
-// /usr/include/c++/9/bits/vector.tcc:659:48:   required from ‘void std::vector<_Tp, _Alloc>::_M_default_append(std::vector<_Tp, _Alloc>::size_type) [with _Tp = Highs; _Alloc = std::allocator<Highs>; std::vector<_Tp, _Alloc>::size_type = long unsigned int]’
-// /usr/include/c++/9/bits/stl_vector.h:937:4:   required from ‘void std::vector<_Tp, _Alloc>::resize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = Highs; _Alloc = std::allocator<Highs>; std::vector<_Tp, _Alloc>::size_type = long unsigned int]’
-// /home/jajhall/HiGHS/check/TestParallel.cpp:98:39:   required from here
-// /usr/include/c++/9/bits/stl_uninitialized.h:127:72: error: static assertion failed: result type must be constructible from value type of input range
-//   127 |       static_assert(is_constructible<_ValueType2, decltype(*__first)>::value,
-//       |
-  
-  /*
-  std::vector<Highs> parallel_highs;
-  parallel_highs.resize(num_lp_solvers);
-  for (HighsInt lp_solver = 0; lp_solver < num_lp_solvers; lp_solver++) {
-    parallel_highs[lp_solver].passModel(lp);
-  }
-  */
-  
-  // Alternatively this should work too:
-  //
-  // @Leona: Yes, it does
+
+  const HighsInt primal_simplex_index = 0;
+  const HighsInt dual_simplex_index = 1;
+  const HighsInt ipm_index = 2;
+
+  assert(lp_solvers[primal_simplex_index] == kLpSolverPrimalSimplex);
+  assert(lp_solvers[dual_simplex_index] == kLpSolverDualSimplex);
+  assert(lp_solvers[ipm_index] == kLpSolverInteriorPoint);
+
+  // Set up a vector of pointers to Highs instances 
   vector<std::unique_ptr<Highs>> parallel_highs;
-  for (HighsInt lp_solver = 0; lp_solver< num_lp_solvers; lp_solver++) {
+  for (HighsInt lp_solver = 0; lp_solver< num_lp_solvers; lp_solver++)
     parallel_highs.emplace_back(new Highs());
-    parallel_highs[lp_solver]->passModel(lp);
-  }
 
    std::vector<double> run_time(num_lp_solvers);
    std::vector<double> objective_function_value(num_lp_solvers);
    std::vector<HighsStatus> run_status(num_lp_solvers);
+
+   // Force the primal simplex instance to use primal simplex
+   parallel_highs[primal_simplex_index]->setOptionValue("simplex_strategy", kSimplexStrategyDual);
+   // Force the dual simplex instance to use dual simplex
+   parallel_highs[dual_simplex_index]->setOptionValue("simplex_strategy", kSimplexStrategyPrimal);
+   // Force the IPM instance to use IPM
+   parallel_highs[ipm_index]->setOptionValue("solver", kIpmString);
+
    run_status.assign(num_lp_solvers, HighsStatus::kError);
-  // when you have this as a vector you can also use the parallel::for_each call
+   // when you have this as a vector you can also use the parallel::for_each call
    parallel::for_each(0, num_lp_solvers, [&](HighsInt start, HighsInt end){
     for (HighsInt lp_solver = start; lp_solver < end; ++lp_solver)
     {
@@ -161,7 +147,7 @@ double concurrentLpSolve(const bool use_race_timer) {
       
       // todo call race_timer decreaseLimit() function if solved successfully
     }
-  });
+					 });
   printf("Status values: primal = %s; dual = %s; ipm = %s\n",
          highsStatusToString(run_status[0]).c_str(),
          highsStatusToString(run_status[1]).c_str(),
@@ -177,51 +163,7 @@ double concurrentLpSolve(const bool use_race_timer) {
   // you can remove the for-loop and change "[&](HighsInt start, HighsInt end){"
   // to "[&](HighsInt lp_solver, HighsInt){"
 
-  //
-  // vector<Highs*> parallel_highs;
-  // for (HighsInt lp_solver = 0; lp_solver< num_lp_solvers; lp_solver++) {
-  // parallel_highs.push_back(new Highs());
-  // parallel_highs[lp_solver]->passModel(lp);
-  // }
-  // parallel_highs[kLpSolverPrimalSimplex]->setOptionValue("simplex_strategy",
-  // kSimplexStrategyPrimal);
 
-  Highs highs_primal_simplex;
-  Highs highs_dual_simplex;
-  Highs highs_ipm;
-  highs_primal_simplex.setOptionValue("output_flag", false);
-  highs_dual_simplex.setOptionValue("output_flag", false);
-  highs_ipm.setOptionValue("output_flag", false);
-
-  HighsStatus primal_simplex_status;
-  HighsStatus dual_simplex_status;
-  HighsStatus ipm_status;
-  double primal_simplex_time;
-  double dual_simplex_time;
-  double ipm_time;
-  double primal_simplex_objective;
-  double dual_simplex_objective;
-  double ipm_objective;
-  //  parallel::spawn([&]() { primal_simplex_status =
-  //  highs_primal_simplex.passModel(lp); }); parallel::sync();
-
-  primal_simplex_status = highs_primal_simplex.passModel(lp);
-  REQUIRE(primal_simplex_status == HighsStatus::kOk);
-
-  dual_simplex_status = highs_dual_simplex.passModel(lp);
-  REQUIRE(dual_simplex_status == HighsStatus::kOk);
-
-  ipm_status = highs_ipm.passModel(lp);
-  REQUIRE(ipm_status == HighsStatus::kOk);
-
-  highs_primal_simplex.setOptionValue("simplex_strategy",
-                                      kSimplexStrategyPrimal);
-  highs_dual_simplex.setOptionValue("simplex_strategy", kSimplexStrategyDual);
-  highs_ipm.setOptionValue("solver", kIpmString);
-
-  primal_simplex_status = HighsStatus::kError;
-  dual_simplex_status = HighsStatus::kError;
-  ipm_status = HighsStatus::kError;
 
   if (use_race_timer) {
     // @Leona I realise that I need to specify a type - double in this
@@ -230,73 +172,55 @@ double concurrentLpSolve(const bool use_race_timer) {
     HighsRaceTimer<double> race_timer;
 
     parallel::TaskGroup tg;
-    tg.spawn([&]() {
-      // todo: somehow pass a pointer to the race_timer into the solver. The
-      // solver should check if such a pointer was passed and call the
-      // limitReached(currentTime) function to determine whether a limit was
-      // reached. If limitReached returns true then the solver should stop.
-      // e.g. something like:
-      // if (race_timer_ptr && race_timer_ptr->limitReached(currentTime))
-      //  modelstatus = HighsModelStatus::kIterationLimit;
+    for (HighsInt lp_solver = 0; lp_solver < num_lp_solvers; lp_solver++) {
+      tg.spawn([&]() {
+		 // todo: somehow pass a pointer to the race_timer into the solver. The
+		 // solver should check if such a pointer was passed and call the
+		 // limitReached(currentTime) function to determine whether a limit was
+		 // reached. If limitReached returns true then the solver should stop.
+		 // e.g. something like:
+		 // if (race_timer_ptr && race_timer_ptr->limitReached(currentTime))
+		 //  modelstatus = HighsModelStatus::kIterationLimit;
 
-      primal_simplex_status = highs_primal_simplex.run();
-      // this should check for the status returned when the race timer limit was
-      // reached and only call decrease limit if it was not reached
-      if (highs_dual_simplex.getModelStatus() !=
-          HighsModelStatus::kIterationLimit)
-        race_timer.decreaseLimit(highs_primal_simplex.getHighsRunTime());
-    });
-    tg.spawn([&]() {
-      dual_simplex_status = highs_dual_simplex.run();
-      if (highs_dual_simplex.getModelStatus() !=
-          HighsModelStatus::kIterationLimit)
-        race_timer.decreaseLimit(highs_primal_simplex.getHighsRunTime());
-    });
-    tg.spawn([&]() {
-      ipm_status = highs_ipm.run();
-      // this should check for the status returned when the race timer limit was
-      // reached and only call decrease limit if it was not reached
-      if (highs_dual_simplex.getModelStatus() !=
-          HighsModelStatus::kIterationLimit)
-        race_timer.decreaseLimit(highs_primal_simplex.getHighsRunTime());
-    });
-
+		 run_status[lp_solver] = parallel_highs[lp_solver]->run();
+		 // this should check for the status returned when the race timer limit was
+		 // reached and only call decrease limit if it was not reached
+		 if (parallel_highs[lp_solver]->getModelStatus() !=
+		     HighsModelStatus::kIterationLimit)
+		   race_timer.decreaseLimit(parallel_highs[lp_solver]->getRunTime());
+	       });
+    }
     tg.taskWait();
   } else {
     parallel::TaskGroup tg;
-    tg.spawn([&]() { primal_simplex_status = highs_primal_simplex.run(); });
-    tg.spawn([&]() { dual_simplex_status = highs_dual_simplex.run(); });
-    tg.spawn([&]() { ipm_status = highs_ipm.run(); });
+    
+    tg.spawn([&]() { run_status[primal_simplex_index] = parallel_highs[primal_simplex_index]->run(); });
+    tg.spawn([&]() { run_status[dual_simplex_index] = parallel_highs[dual_simplex_index]->run(); });
+    tg.spawn([&]() { run_status[ipm_index] = parallel_highs[ipm_index]->run(); });
 
-    tg.sync();
-    tg.sync();
-    tg.sync();
-
-    // alternatively  instead of the 3 sync() calls you can call: tg.taskWait();
+    tg.taskWait();
+  }
+  double total_time = 0;
+  for (HighsInt lp_solver = 0; lp_solver < num_lp_solvers; lp_solver++) {
+    objective_function_value[lp_solver] = parallel_highs[lp_solver]->getInfo().objective_function_value;
+    run_time[lp_solver] = parallel_highs[lp_solver]->getRunTime();
+    total_time += run_time[lp_solver];
   }
 
-  primal_simplex_time = highs_primal_simplex.getRunTime();
-  dual_simplex_time = highs_dual_simplex.getRunTime();
-  ipm_time = highs_ipm.getRunTime();
-
-  primal_simplex_objective =
-      highs_primal_simplex.getInfo().objective_function_value;
-  dual_simplex_objective =
-      highs_dual_simplex.getInfo().objective_function_value;
-  ipm_objective = highs_ipm.getInfo().objective_function_value;
-
   printf("Status values: primal = %s; dual = %s; ipm = %s\n",
-         highsStatusToString(primal_simplex_status).c_str(),
-         highsStatusToString(dual_simplex_status).c_str(),
-         highsStatusToString(ipm_status).c_str());
+         highsStatusToString(run_status[0]).c_str(),
+         highsStatusToString(run_status[1]).c_str(),
+         highsStatusToString(run_status[2]).c_str());
 
   printf("Solve times:  primal = %f6.4; dual = %f6.4; ipm = %f6.4\n",
-         primal_simplex_time, dual_simplex_time, ipm_time);
+         run_time[0], run_time[1], run_time[2]);
 
-  REQUIRE(equalObjective(dual_simplex_objective, primal_simplex_objective));
-  REQUIRE(equalObjective(dual_simplex_objective, ipm_objective));
+  REQUIRE(equalObjective(objective_function_value[dual_simplex_index],
+			 objective_function_value[primal_simplex_index]));
+  REQUIRE(equalObjective(objective_function_value[dual_simplex_index],
+			 objective_function_value[ipm_index]));
 
-  return primal_simplex_time + dual_simplex_time + ipm_time;
+  return total_time;
 }
 
 bool equalObjective(const double obj0, const double obj1) {
