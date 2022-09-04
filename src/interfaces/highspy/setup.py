f77a4b39f279b98d1a16d81d4f40572371016b03
Ivet Galabova <galabovaa@gmail.com>
2022-09-04 13:54:26 +0300

    version bump to allow upload

--- src/interfaces/highspy/setup.py
+++ src/interfaces/highspy/setup.py
@@ -27,7 +27,7 @@ try:
                                         libraries=['highs']))
     
     setup(name='highspy',
-          version='1.1.2.dev1',
+          version='1.1.2.dev2',
           packages=find_packages(),
           description='Python interface to HiGHS',
           maintainer_email='highsopt@gmail.com',
