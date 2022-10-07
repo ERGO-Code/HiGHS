02680b1cef735046cbb17334e0f9a21bc949239b
Julian Hall <jajhall@ed.ac.uk>
2022-10-07 18:01:04 +0100

    Added ../check/TestRaceTimer.cpp

---
+++ check/TestRaceTimer.cpp
@@ -0,0 +1,53 @@
+#include "Highs.h"
+#include "catch.hpp"
+//#include "parallel/HighsParallel.h"
+#include "parallel/HighsRaceTimer.h"
+
+const bool dev_run = true;
+
+void testSolver(Highs& highs, const std::string solver,
+                const HighsInt simplex_strategy, const double time_limit) {
+  highs.setBasis();
+  highs.setSolution();
+  highs.setOptionValue("solver", solver);
+  if (simplex_strategy == kSimplexStrategyDualPlain)
+    highs.setOptionValue("parallel", kHighsOffString);
+  if (simplex_strategy >= 0)
+    highs.setOptionValue("simplex_strategy", simplex_strategy);
+  double run_time = -highs.getRunTime();
+  HighsStatus return_status = highs.run();
+  run_time += highs.getRunTime();
+  printf(
+      "Running for %11.4gs (limit %11.4g) with \"%s\" (strategy %d) returns "
+      "\"%s\" and model status \"%s\"\n",
+      run_time, time_limit, solver.c_str(), int(simplex_strategy),
+      highsStatusToString(return_status).c_str(),
+      highs.modelStatusToString(highs.getModelStatus()).c_str());
+}
+
+TEST_CASE("test-race-timer", "[highs_test_race_timer]") {
+  Highs highs;
+  highs.setOptionValue("output_flag", dev_run);
+  std::string model_file;
+  model_file = std::string(HIGHS_DIR) + "/check/instances/25fv47.mps";
+
+  highs.setOptionValue("presolve", kHighsOffString);
+  highs.setOptionValue("solver", kIpmString);
+  highs.readModel(model_file);
+  double time_limit = -highs.getRunTime();
+  highs.run();
+  time_limit += highs.getRunTime();
+  time_limit /= 10;
+  HighsLp lp = highs.getLp();
+
+  HighsRaceTimer<double> race_timer;
+  highs.passRaceTimer(&race_timer);
+
+  race_timer.decreaseLimit(time_limit);
+
+  testSolver(highs, kIpmString, -1, time_limit);
+  testSolver(highs, kSimplexString, 1, time_limit);
+  testSolver(highs, kSimplexString, 2, time_limit);
+  testSolver(highs, kSimplexString, 3, time_limit);
+  testSolver(highs, kSimplexString, 4, time_limit);
+}
