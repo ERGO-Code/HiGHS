5c8fc4ca9c8c89f3f89b3be73fb41137fb22d046
jannick.lange <jannick.lange@optano.com>
2022-10-14 10:01:10 +0200

    Add new ctor param to c# example. Remove '.dll' from lib name, so that .net interface will run platform independent

--- src/interfaces/highs_csharp_api.cs
+++ src/interfaces/highs_csharp_api.cs
@@ -148,7 +148,7 @@ public class HighsLpSolver : IDisposable
 
    private bool _disposed;
 
-   private const string highslibname = "libhighs.dll";
+   private const string highslibname = "libhighs";
 
    [DllImport(highslibname)]
    private static extern int Highs_call(
