b82aa6b9bfc19a174c06ffef856b2765efdb0298
Julian Hall <jajhall@ed.ac.uk>
2022-10-20 11:36:11 +0100

    Checked write-read .lp files and now using offset

--- src/io/FilereaderLp.cpp
+++ src/io/FilereaderLp.cpp
@@ -71,7 +71,7 @@ FilereaderRetcode FilereaderLp::readModelFromFile(const HighsOptions& options,
     lp.objective_name_ = m.objective->name;
     // ToDo: Fix m.objective->offset and then use it here
     //
-    // lp.offset_ = m.objective->offset;
+    lp.offset_ = m.objective->offset;
     lp.col_cost_.resize(lp.num_col_, 0.0);
     for (HighsUInt i = 0; i < m.objective->linterms.size(); i++) {
       std::shared_ptr<LinTerm> lt = m.objective->linterms[i];
