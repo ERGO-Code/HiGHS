278197003c7c3414761b15418231f284cb552857
Ivet Galabova <galabovaa@gmail.com>
2022-11-08 13:43:31 +0200

    Presolve later

--- src/presolve/HPresolve.cpp
+++ src/presolve/HPresolve.cpp
@@ -29,6 +29,7 @@
 #include "mip/HighsObjectiveFunction.h"
 #include "pdqsort/pdqsort.h"
 #include "presolve/HighsPostsolveStack.h"
+#include "test/DevKkt.h"
 #include "util/HFactor.h"
 #include "util/HighsCDouble.h"
 #include "util/HighsIntegers.h"
