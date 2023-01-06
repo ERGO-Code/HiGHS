65d0b7b16bbe19f97ea5432f73e42ab74df94be1
JAJHall <jajhall@ed.ac.uk>
2023-01-06 16:56:43 +0000

    Formatted

--- src/Highs.h
+++ src/Highs.h
@@ -84,7 +84,10 @@ class Highs {
    */
   std::string compilationDate() const { return highsCompilationDate(); }
 
-  HighsInt foo(int &i) { i++; return 123; }
+  HighsInt foo(int& i) {
+    i++;
+    return 123;
+  }
 
   /**
    * @brief Reset the options and then call clearModel()
