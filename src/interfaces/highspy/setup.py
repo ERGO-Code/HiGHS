f797c1ab602004004186d745cbfce83f50d39932
JAJHall <jajhall@ed.ac.uk>
2023-02-01 11:45:40 +0000

    Removed print statement from HFactor.cpp

--- src/interfaces/highspy/setup.py
+++ src/interfaces/highspy/setup.py
@@ -27,7 +27,7 @@ try:
                                         libraries=['highs']))
     
     setup(name='highspy',
-          version='1.4.0.dev0',
+          version='1.4.2',
           packages=find_packages(),
           description='Python interface to HiGHS',
           maintainer_email='highsopt@gmail.com',
