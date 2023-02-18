a20096995821cb20e7e0df9a1fe63c7b6293a827
JAJHall <jajhall@ed.ac.uk>
2023-02-18 15:05:23 +0000

    Incorporated @rschwarz mods - and added [5] to char def in later loops; LP file readre now uses model names

--- check/TestCAPI.c
+++ check/TestCAPI.c
@@ -8,9 +8,7 @@
 #include <math.h>
 #include <string.h>
 
-const HighsInt dev_run = 1;
-const HighsInt full_api_names = 0;
-const HighsInt full_api_mip_integrality = 1;
+const HighsInt dev_run = 0;
 const double double_equal_tolerance = 1e-5;
 
 HighsInt intArraysEqual(const HighsInt dim, const HighsInt* array0, const HighsInt* array1) {
@@ -439,61 +437,57 @@ void full_api() {
   return_status = Highs_writeModel(highs, "");
   assert( return_status == kHighsStatusError );
 
-  if (full_api_names) {
-    // Define all column names to be different
-    //
-    // Executing this loop leads to CI failures - capi_unit_tests
-    // times out???
-    for (HighsInt iCol = 0; iCol < num_col; iCol++) {
-      const char suffix = iCol + '0';
-      const char* suffix_p = &suffix;
-      char name[4];
-      strncpy(name, col_prefix, sizeof(name));
-      strncat(name, suffix_p, sizeof(name)-strlen(name));
-      const char* name_p = name;
-      return_status = Highs_passColName(highs, iCol, name_p);
-      assert( return_status == kHighsStatusOk );
-    }
-    return_status = Highs_writeModel(highs, "");
+  // Define all column names to be different
+  //
+  // Executing this loop leads to CI failures - capi_unit_tests
+  // times out???
+  for (HighsInt iCol = 0; iCol < num_col; iCol++) {
+    const char suffix = iCol + '0';
+    const char* suffix_p = &suffix;
+    char name[5];  // 3 chars prefix, 1 char iCol, 1 char 0-terminator
+    sprintf(name, "%s%d", col_prefix, iCol);
+    const char* name_p = name;
+    return_status = Highs_passColName(highs, iCol, name_p);
     assert( return_status == kHighsStatusOk );
-
-    // Define all row names to be the same
-    for (HighsInt iRow = 0; iRow < num_row; iRow++) {
-      return_status = Highs_passRowName(highs, iRow, row_prefix);
-      assert( return_status == kHighsStatusOk );
-    }
-    return_status = Highs_writeModel(highs, "");
-    assert( return_status == kHighsStatusError );
-
-    // Define all row names to be different
-    for (HighsInt iRow = 0; iRow < num_row; iRow++) {
-      const char suffix = iRow + '0';
-      const char* suffix_p = &suffix;
-      char name[4];
-      strncpy(name, row_prefix, sizeof(name));
-      strncat(name, suffix_p, sizeof(name)-strlen(name));
-      const char* name_p = name;
-      return_status = Highs_passRowName(highs, iRow, name_p);
-      assert( return_status == kHighsStatusOk );
-    }
-    return_status = Highs_writeModel(highs, "");
+  }
+  return_status = Highs_writeModel(highs, "");
+  assert( return_status == kHighsStatusOk );
+  
+  // Define all row names to be the same
+  for (HighsInt iRow = 0; iRow < num_row; iRow++) {
+    return_status = Highs_passRowName(highs, iRow, row_prefix);
     assert( return_status == kHighsStatusOk );
-
-    for (HighsInt iCol = 0; iCol < num_col; iCol++) {
-      char name[4];
-      char* name_p = name;
-      return_status = Highs_getColName(highs, iCol, name_p);
-      assert( return_status == kHighsStatusOk );
-      if (dev_run) printf("Column %"HIGHSINT_FORMAT" has name %s\n", iCol, name_p);
-    }
-
-    for (HighsInt iRow = 0; iRow < num_row; iRow++) {
-      char name[4];
-      char* name_p = name;
-      return_status = Highs_getRowName(highs, iRow, name_p);
-      assert( return_status == kHighsStatusOk );
-      if (dev_run) printf("Row    %"HIGHSINT_FORMAT" has name %s\n", iRow, name_p);
-    }
+  }
+  return_status = Highs_writeModel(highs, "");
+  assert( return_status == kHighsStatusError );
+  
+  // Define all row names to be different
+  for (HighsInt iRow = 0; iRow < num_row; iRow++) {
+    const char suffix = iRow + '0';
+    const char* suffix_p = &suffix;
+    char name[5];  // 3 chars prefix, 1 char iCol, 1 char 0-terminator
+    sprintf(name, "%s%d", row_prefix, iRow);
+    const char* name_p = name;
+    return_status = Highs_passRowName(highs, iRow, name_p);
+    assert( return_status == kHighsStatusOk );
+  }
+  return_status = Highs_writeModel(highs, "");
+  assert( return_status == kHighsStatusOk );
+  
+  for (HighsInt iCol = 0; iCol < num_col; iCol++) {
+    char name[5];
+    char* name_p = name;
+    return_status = Highs_getColName(highs, iCol, name_p);
+    assert( return_status == kHighsStatusOk );
+    if (dev_run) printf("Column %"HIGHSINT_FORMAT" has name %s\n", iCol, name_p);
+  }
+  
+  for (HighsInt iRow = 0; iRow < num_row; iRow++) {
+    char name[5];
+    char* name_p = name;
+    return_status = Highs_getRowName(highs, iRow, name_p);
+    assert( return_status == kHighsStatusOk );
+    if (dev_run) printf("Row    %"HIGHSINT_FORMAT" has name %s\n", iRow, name_p);
   }
 
   Highs_destroy(highs);
@@ -901,18 +895,16 @@ void full_api_mip() {
   assert( return_status == kHighsStatusOk );
   assert( mip_node_count == 1 );
 
-  if (full_api_mip_integrality) {
-    // Test Highs_getColIntegrality
-    HighsInt col_integrality;
-    return_status = Highs_getColIntegrality(highs, -1, &col_integrality);
-    assert( return_status == kHighsStatusError );
-    return_status = Highs_getColIntegrality(highs, num_col, &col_integrality);
-    assert( return_status == kHighsStatusError );
-    for (HighsInt iCol = 0; iCol < num_col; iCol++) {
-      return_status = Highs_getColIntegrality(highs, iCol, &col_integrality);
-      assert( return_status == kHighsStatusOk );
-      assert( col_integrality == 1 );
-    }
+  // Test Highs_getColIntegrality
+  HighsInt col_integrality;
+  return_status = Highs_getColIntegrality(highs, -1, &col_integrality);
+  assert( return_status == kHighsStatusError );
+  return_status = Highs_getColIntegrality(highs, num_col, &col_integrality);
+  assert( return_status == kHighsStatusError );
+  for (HighsInt iCol = 0; iCol < num_col; iCol++) {
+    return_status = Highs_getColIntegrality(highs, iCol, &col_integrality);
+    assert( return_status == kHighsStatusOk );
+    assert( col_integrality == 1 );
   }
 
   Highs_destroy(highs);
--- src/io/FilereaderLp.cpp
+++ src/io/FilereaderLp.cpp
@@ -23,7 +23,7 @@
 #include "lp_data/HighsLpUtils.h"
 
 const bool original_double_format = false;
-const bool allow_model_names = false;
+const bool allow_model_names = true;
 
 FilereaderRetcode FilereaderLp::readModelFromFile(const HighsOptions& options,
                                                   const std::string filename,
@@ -281,9 +281,9 @@ HighsStatus FilereaderLp::writeModelToFile(const HighsOptions& options,
   ar_matrix.ensureRowwise();
 
   const bool has_col_names =
-      allow_model_names && lp.col_names_.size() == lp.num_col_;
+      allow_model_names && HighsInt(lp.col_names_.size()) == lp.num_col_;
   const bool has_row_names =
-      allow_model_names && lp.row_names_.size() == lp.num_row_;
+      allow_model_names && HighsInt(lp.row_names_.size()) == lp.num_row_;
   FILE* file = fopen(filename.c_str(), "w");
 
   // write comment at the start of the file
