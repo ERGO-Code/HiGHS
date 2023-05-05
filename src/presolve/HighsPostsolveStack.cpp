29498ab2a00707a575c984c8d1e2a01d122f934a
JAJHall <jajhall@ed.ac.uk>
2023-05-05 12:48:10 +0100

    Improved the asserts in HighsPostsolveStack::DuplicateColumn::undo

--- src/presolve/HighsPostsolveStack.cpp
+++ src/presolve/HighsPostsolveStack.cpp
@@ -671,10 +671,34 @@ void HighsPostsolveStack::DuplicateColumn::undo(const HighsOptions& options,
       basis.col_status[col] = HighsBasisStatus::kLower;
     }
   }
-  assert(solution.col_value[duplicateCol] >= duplicateColLower);
-  assert(solution.col_value[duplicateCol] <= duplicateColUpper);
-  assert(solution.col_value[col] >= colLower);
-  assert(solution.col_value[col] <= colUpper);
+  if (solution.col_value[duplicateCol] < duplicateColLower) {
+    double residual = duplicateColLower - solution.col_value[duplicateCol];
+    assert(residual <= options.mip_feasibility_tolerance);
+    assert(residual <= options.mip_feasibility_tolerance / 1e2);
+    assert(residual <= options.mip_feasibility_tolerance / 1e4);
+    assert(residual <= 0);
+  }
+  if (solution.col_value[duplicateCol] > duplicateColUpper) {
+    double residual = solution.col_value[duplicateCol] - duplicateColUpper;
+    assert(residual <= options.mip_feasibility_tolerance);
+    assert(residual <= options.mip_feasibility_tolerance / 1e2);
+    assert(residual <= options.mip_feasibility_tolerance / 1e4);
+    assert(residual <= 0);
+  }
+  if (solution.col_value[col] < colLower) {
+    double residual = colLower - solution.col_value[col];
+    assert(residual <= options.mip_feasibility_tolerance);
+    assert(residual <= options.mip_feasibility_tolerance / 1e2);
+    assert(residual <= options.mip_feasibility_tolerance / 1e4);
+    assert(residual <= 0);
+  }
+  if (solution.col_value[col] > colUpper) {
+    double residual = solution.col_value[col] - colUpper;
+    assert(residual <= options.mip_feasibility_tolerance);
+    assert(residual <= options.mip_feasibility_tolerance / 1e2);
+    assert(residual <= options.mip_feasibility_tolerance / 1e4);
+    assert(residual <= 0);
+  }
 }
 
 void HighsPostsolveStack::DuplicateColumn::transformToPresolvedSpace(
