7089ab64a9546f6fa8c16393c0f2091b30275502
Stefan Vigerske <svigerske@gams.com>
2023-05-29 17:06:50 +0200

    pass log callback data to log callback again
    - and undeprecate log callback data argument
    - undoes e002eca1b and previous renamings

--- src/io/HighsIO.cpp
+++ src/io/HighsIO.cpp
@@ -135,7 +135,7 @@ void highsLogUser(const HighsLogOptions& log_options_, const HighsLogType type,
       // Output was truncated: for now just ensure string is null-terminated
       msgbuffer[sizeof(msgbuffer) - 1] = '\0';
     }
-    log_options_.log_user_callback(type, msgbuffer, nullptr);
+    log_options_.log_user_callback(type, msgbuffer, log_options_.log_user_callback_data);
   }
   va_end(argptr);
 }
@@ -184,7 +184,7 @@ void highsLogDev(const HighsLogOptions& log_options_, const HighsLogType type,
       // Output was truncated: for now just ensure string is null-terminated
       msgbuffer[sizeof(msgbuffer) - 1] = '\0';
     }
-    log_options_.log_user_callback(type, msgbuffer, nullptr);
+    log_options_.log_user_callback(type, msgbuffer, log_options_.log_user_callback_data);
   }
   va_end(argptr);
 }
