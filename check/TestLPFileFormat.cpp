edda5d91044698976c7ff381973d67cd8c78d339
jajhall <jajhall@ed.ac.uk>
2023-07-31 15:43:40 +0100

    Test file is now qcqp.lp, not quadratic.lp; now testing that Highs::readModel returns kError for it

--- check/TestLPFileFormat.cpp
+++ check/TestLPFileFormat.cpp
@@ -3,21 +3,20 @@
 #include "catch.hpp"
 #include "filereaderlp/reader.hpp"
 
-const bool dev_run = false;
+const bool dev_run = true;
 
 TEST_CASE("lp-file-format-quad-no-space", "[LpFileFormat]") {
-  std::string filename =
-      std::string(HIGHS_DIR) + "/check/instances/quadratic.lp";
+  std::string filename = std::string(HIGHS_DIR) + "/check/instances/qcqp.lp";
 
-  //HiGHS cannot handle quadratic constraints as there are in quadratic.lp
-  //so only test that filereaderlp succeeds for now
-  //Highs highs;
-  //highs.setOptionValue("output_flag", dev_run);
-  //REQUIRE(highs.readModel(filename) == HighsStatus::kOk);
+  // HiGHS cannot handle quadratic constraints as there are in qcqp.lp
 
-  // lpassert in extern/filereaderlp/def.hpp throws
-  // std::invalid_argument whatever the error
+  // Test that filereaderlp does not throw an exception
   REQUIRE_NOTHROW(readinstance(filename));
+
+  // Test that HiGHS returns an error when passes a QCQP problem
+  Highs highs;
+  highs.setOptionValue("output_flag", dev_run);
+  REQUIRE(highs.readModel(filename) == HighsStatus::kError);
 
   // todo: read the coefficients/variables off the highs model
 }
