c7e752d17b87681255639745d39322a466725ee1
jajhall <jajhall@ed.ac.uk>
2023-10-09 15:31:19 +0100

    Setting lp.has_infinite_cost_ isn't enough

--- src/lp_data/HighsInterface.cpp
+++ src/lp_data/HighsInterface.cpp
@@ -1637,22 +1637,31 @@ HighsStatus Highs::handleInfCost() {
       }
     }
   }
+  // Infinite costs have been removed, but their presence in the
+  // original model is known from mods.save_inf_cost_variable_*, so
+  // set lp.has_infinite_cost_ to be false to avoid assert when run()
+  // is called using copy of model in MIP solver (See #1446)
+  lp.has_infinite_cost_ = false;
+  
   return HighsStatus::kOk;
 }
 
 void Highs::restoreInfCost(HighsStatus& return_status) {
   HighsLp& lp = this->model_.lp_;
-  if (!lp.has_infinite_cost_) return;
   HighsBasis& basis = this->basis_;
   HighsLpMods& mods = lp.mods_;
   HighsInt num_inf_cost = mods.save_inf_cost_variable_index.size();
+  if (num_inf_cost <= 0) return;
   assert(num_inf_cost);
   for (HighsInt ix = 0; ix < num_inf_cost; ix++) {
     HighsInt iCol = mods.save_inf_cost_variable_index[ix];
     double cost = mods.save_inf_cost_variable_cost[ix];
     double lower = mods.save_inf_cost_variable_lower[ix];
     double upper = mods.save_inf_cost_variable_upper[ix];
+    HighsBasisStatus status = basis.valid ? basis.col_status[iCol] : HighsBasisStatus::kBasic;
     double value = solution_.value_valid ? solution_.col_value[iCol] : 0;
+    printf("Highs::restoreInfCost ix = %d; iCol = %d / %d; cost = %g; [lower, value, upper] = [%g, %g, %g] Basis status = %s\n",
+	   int(ix), int(iCol), int(lp.num_col_), cost, lower, value, upper, basis.valid ? utilBasisStatusToString(status).c_str() : "No basis");
     if (basis.valid) {
       assert(basis.col_status[iCol] != HighsBasisStatus::kBasic);
       if (lp.col_lower_[iCol] == lower) {
@@ -1667,6 +1676,9 @@ void Highs::restoreInfCost(HighsStatus& return_status) {
     lp.col_lower_[iCol] = lower;
     lp.col_upper_[iCol] = upper;
   }
+  // Infinite costs have been reintroduced, so reset to true the flag
+  // that was set false in Highs::handleInfCost() (See #1446)
+  lp.has_infinite_cost_ = true;
 
   if (this->model_status_ == HighsModelStatus::kInfeasible) {
     // Model is infeasible with the infinite cost variables fixed at
