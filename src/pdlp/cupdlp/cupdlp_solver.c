7b1b3da94e28989ff128678b01e331cfedf50fc1
JAJHall <jajhall@ed.ac.uk>
2024-02-05 18:56:13 +0000

    Integrated cuPDLP-C main

--- src/pdlp/cupdlp/cupdlp_solver.c
+++ src/pdlp/cupdlp/cupdlp_solver.c
@@ -1141,7 +1141,8 @@ cupdlp_retcode LP_SolvePDHG(
                              constraint_type, col_value, col_dual, row_value,
                              row_dual, value_valid, dual_valid));
 
-  writeJson(fp, pdhg);
+  if (fp)
+    writeJson(fp, pdhg);
 
   if (ifSaveSol && fp_sol) {
     if (strcmp(fp, fp_sol) != 0) {
