15d8d1ba98e625dfc46438a10808e528cb229317
jajhall <jajhall@ed.ac.uk>
2024-02-09 17:12:50 +0000

    Removed cupdlp_mps*

--- src/pdlp/cupdlp/cupdlp.h
+++ src/pdlp/cupdlp/cupdlp.h
@@ -5,7 +5,6 @@
 #include "cupdlp_cs.h"
 #include "cupdlp_defs.h"
 #include "cupdlp_linalg.h"
-#include "cupdlp_mps.h"
 #include "cupdlp_proj.h"
 #include "cupdlp_restart.h"
 #include "cupdlp_scaling_cuda.h"
