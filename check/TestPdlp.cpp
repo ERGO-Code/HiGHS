983cf278c0718d355bdf8de6e775c5b18cc70b8c
JAJHall <jajhall@ed.ac.uk>
2024-02-15 09:48:03 +0000

    Investigating unit test failure

--- check/TestPdlp.cpp
+++ check/TestPdlp.cpp
@@ -15,7 +15,7 @@ TEST_CASE("pdlp-distillation-lp", "[pdlp]") {
   special_lps.distillationLp(lp, require_model_status, optimal_objective);
 
   Highs highs;
-  highs.setOptionValue("output_flag", dev_run);
+  //  highs.setOptionValue("output_flag", dev_run);
   const HighsInfo& info = highs.getInfo();
   const HighsOptions& options = highs.getOptions();
   REQUIRE(highs.passModel(lp) == HighsStatus::kOk);
@@ -47,13 +47,19 @@ TEST_CASE("pdlp-distillation-lp", "[pdlp]") {
     }
   }
   HighsInt pdlp_iteration_count = highs.getInfo().pdlp_iteration_count;
+  REQUIRE(pdlp_iteration_count > 0);
+  REQUIRE(pdlp_iteration_count == 160);
   // Now run with half the iteration count as the limit to test
   // iteration limit termination
+
   highs.setOptionValue("pdlp_iteration_limit", pdlp_iteration_count / 2);
   run_status = highs.run();
 
   REQUIRE(run_status == HighsStatus::kWarning);
   REQUIRE(highs.getModelStatus() == HighsModelStatus::kIterationLimit);
+  pdlp_iteration_count = highs.getInfo().pdlp_iteration_count;
+  REQUIRE(pdlp_iteration_count > 0);
+  REQUIRE(pdlp_iteration_count == 79);
 }
 
 TEST_CASE("pdlp-3d-lp", "[pdlp]") {
