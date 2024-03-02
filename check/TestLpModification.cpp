6e4bf3308f710d45f9e344d8d2ba4927ded94dca
JAJHall <jajhall@ed.ac.uk>
2024-03-02 20:17:59 +0000

    Another try

--- check/TestLpModification.cpp
+++ check/TestLpModification.cpp
@@ -1113,19 +1113,25 @@ TEST_CASE("LP-getcols", "[highs_data]") {
                 nullptr, nullptr, nullptr);
   REQUIRE(num_cols == 1);
   REQUIRE(num_nz == 1);
-  // Make sure this works for getting no columns from an interval
-  if (dev_run) printf("Get no columns from an interval\n");
 
-  highs.getCols(1, 0, num_cols, nullptr, nullptr, nullptr, num_nz, nullptr,
-                nullptr, nullptr);
-  REQUIRE(num_cols == 0);
-  REQUIRE(num_nz == 0);
-  // Make sure this works for getting no columns from a set
-  if (dev_run) printf("Get no columns from a set\n");
-  highs.getCols(0, nullptr, num_cols, nullptr, nullptr, nullptr, num_nz,
-                nullptr, nullptr, nullptr);
-  REQUIRE(num_cols == 0);
-  REQUIRE(num_nz == 0);
+  // Link error on some architectures, since 1 seems indistinguishable
+  // from the pointer to a set
+  HighsInt to_col = 0;
+  const bool try_null_test = true;
+  if (try_null_test) {
+    // Make sure this works for getting no columns from an interval
+    if (dev_run) printf("Get no columns from an interval\n");
+    highs.getCols(1, to_col, num_cols, nullptr, nullptr, nullptr, num_nz,
+                  nullptr, nullptr, nullptr);
+    REQUIRE(num_cols == 0);
+    REQUIRE(num_nz == 0);
+    // Make sure this works for getting no columns from a set
+    if (dev_run) printf("Get no columns from a set\n");
+    highs.getCols(0, nullptr, num_cols, nullptr, nullptr, nullptr, num_nz,
+                  nullptr, nullptr, nullptr);
+    REQUIRE(num_cols == 0);
+    REQUIRE(num_nz == 0);
+  }
 }
 
 TEST_CASE("LP-getrows", "[highs_data]") {
@@ -1170,18 +1176,24 @@ TEST_CASE("LP-getrows", "[highs_data]") {
                 nullptr, nullptr);
   REQUIRE(num_rows == 1);
   REQUIRE(num_nz == 1);
-  // Make sure this works for getting no rows from an interval
-  if (dev_run) printf("Get no rows from an interval\n");
-  highs.getRows(1, 0, num_rows, nullptr, nullptr, num_nz, nullptr, nullptr,
-                nullptr);
-  REQUIRE(num_rows == 0);
-  REQUIRE(num_nz == 0);
-  // Make sure this works for getting no rows from a set
-  if (dev_run) printf("Get no rows from a set\n");
-  highs.getRows(0, nullptr, num_rows, nullptr, nullptr, num_nz, nullptr,
-                nullptr, nullptr);
-  REQUIRE(num_rows == 0);
-  REQUIRE(num_nz == 0);
+  // Link error on some architectures, since 1 seems indistinguishable
+  // from the pointer to a set
+  HighsInt to_row = 0;
+  const bool try_null_test = true;
+  if (try_null_test) {
+    // Make sure this works for getting no rows from an interval
+    if (dev_run) printf("Get no rows from an interval\n");
+    highs.getRows(1, to_row, num_rows, nullptr, nullptr, num_nz, nullptr,
+                  nullptr, nullptr);
+    REQUIRE(num_rows == 0);
+    REQUIRE(num_nz == 0);
+    // Make sure this works for getting no rows from a set
+    if (dev_run) printf("Get no rows from a set\n");
+    highs.getRows(0, nullptr, num_rows, nullptr, nullptr, num_nz, nullptr,
+                  nullptr, nullptr);
+    REQUIRE(num_rows == 0);
+    REQUIRE(num_nz == 0);
+  }
 }
 
 TEST_CASE("LP-interval-changes", "[highs_data]") {
