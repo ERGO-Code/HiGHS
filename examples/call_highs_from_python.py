df4c5b61895458a34a0a5bc478f07b3696d948b2
galabovaa <galabovaa@gmail.com>
2024-03-05 00:30:24 +0000

    style

--- examples/call_highs_from_python.py
+++ examples/call_highs_from_python.py
@@ -1,10 +1,6 @@
-# NB Must have installed highspy, either by using pip install highspy,
-# or by following the instructions on
-# https://github.com/ERGO-Code/HiGHS#python
-#
 # The paths to MPS file instances assumes that this is run in the
-# directory of this file (ie highs/examples) or any other subdirectory
-# of HiGHS
+# root directory of HiGHS
+
 import numpy as np
 import highspy
 import highspy.cb as hscb
@@ -12,8 +8,8 @@ import highspy.cb as hscb
 h = highspy.Highs()
 inf = highspy.kHighsInf
 alt_inf = h.getInfinity()
-print('highspy.kHighsInf = ', inf)
-print('h.getInfinity() = ', alt_inf)
+print('highspy.kHighsInf = ', inf,
+      'h.getInfinity() = ', alt_inf)
 
 # Define a callback
 
@@ -25,11 +21,12 @@ def user_interrupt_callback(
     data_in,
     user_callback_data
 ):
-    dev_run = True  # or any other condition
+    dev_run = True
+    # dev_run = False
 
     # Constants for iteration limits or objective targets, adjust as required
-    ADLITTLE_SIMPLEX_ITERATION_LIMIT = 100
-    ADLITTLE_IPM_ITERATION_LIMIT = 100
+    SIMPLEX_ITERATION_LIMIT = 100
+    IPM_ITERATION_LIMIT = 100
     EGOUT_OBJECTIVE_TARGET = 1.0
 
     # Callback for MIP Improving Solution
@@ -59,36 +56,40 @@ def user_interrupt_callback(
 
         elif callback_type == hscb.HighsCallbackType.kCallbackSimplexInterrupt:
             if dev_run:
-                print(
-                    f"userInterruptCallback(type {callback_type}): {message}")
+                print(f"userInterruptCallback(type {
+                      callback_type}): {message}")
                 print(f"with iteration count = {
-                    data_out.simplex_iteration_count}"
-                )
+                    data_out.simplex_iteration_count}")
+
             data_in.user_interrupt = (
-                data_out.simplex_iteration_count > ADLITTLE_SIMPLEX_ITERATION_LIMIT
+                data_out.simplex_iteration_count > SIMPLEX_ITERATION_LIMIT
             )
 
         elif callback_type == hscb.HighsCallbackType.kCallbackIpmInterrupt:
             if dev_run:
-                print(
-                    f"userInterruptCallback(type {callback_type}): {message} with iteration count = {
-                        data_out.ipm_iteration_count}"
-                )
+                print(f"userInterruptCallback(type {
+                      callback_type}): {message}")
+                print(f"with iteration count = {data_out.ipm_iteration_count}")
+
             data_in.user_interrupt = (
-                data_out.ipm_iteration_count > ADLITTLE_IPM_ITERATION_LIMIT
+                data_out.ipm_iteration_count > IPM_ITERATION_LIMIT
             )
 
         elif callback_type == hscb.HighsCallbackType.kCallbackMipInterrupt:
             if dev_run:
-                print(
-                    f"userInterruptCallback(type {callback_type}): {message} with Bounds ({data_out.mip_dual_bound:.4g}, {
-                        data_out.mip_primal_bound:.4g}); Gap = {data_out.mip_gap:.4g}; Objective = {data_out.objective_function_value:.4g}"
-                )
+                print(f"userInterruptCallback(type {
+                      callback_type}): {message}")
+                print(f"Bounds ({data_out.mip_dual_bound:.4g}, {
+                      data_out.mip_primal_bound:.4g});")
+                print(f"Gap = {data_out.mip_gap:.4g};")
+                print(f"Objective = {data_out.objective_function_value:.4g}")
+
             data_in.user_interrupt = (
                 data_out.objective_function_value < EGOUT_OBJECTIVE_TARGET
             )
 
 
+# Define model
 h.addVar(-inf, inf)
 h.addVar(-inf, inf)
 h.changeColsCost(2, np.array([0, 1]), np.array([0, 1], dtype=np.double))
@@ -101,10 +102,14 @@ indices = np.array([0, 1, 0, 1])
 values = np.array([-1, 1, 1, 1], dtype=np.double)
 h.addRows(num_cons, lower, upper, num_new_nz, starts, indices, values)
 h.setOptionValue("log_to_console", True)
+
+# Set callback and run
 h.setCallback(user_interrupt_callback, None)
 h.startCallback(hscb.HighsCallbackType.kCallbackLogging)
 h.run()
 h.stopCallback(hscb.HighsCallbackType.kCallbackLogging)
+
+# Get solution
 num_var = h.getNumCol()
 solution = h.getSolution()
 basis = h.getBasis()
@@ -124,21 +129,25 @@ for icol in range(num_var):
     print(icol, solution.col_value[icol],
           h.basisStatusToString(basis.col_status[icol]))
 
+
+# ~~~
 # Read in and solve avgas
 h.readModel("check/instances/avgas.mps")
+
 # h.writeModel("ml.mps")
+
 h.run()
 lp = h.getLp()
 num_nz = h.getNumNz()
-print(
-    "LP has ", lp.num_col_, " columns", lp.num_row_, " rows and ", num_nz, " nonzeros"
-)
+print("LP has ", lp.num_col_,
+      " columns", lp.num_row_,
+      " rows and ", num_nz, " nonzeros.")
 
+# ~~~
 # Clear so that incumbent model is empty
 h.clear()
 
 # Now define the blending model as a HighsLp instance
-#
 lp = highspy.HighsLp()
 lp.num_col_ = 2
 lp.num_row_ = 2
@@ -152,7 +161,11 @@ lp.a_matrix_.start_ = np.array([0, 2, 4])
 lp.a_matrix_.index_ = np.array([0, 1, 0, 1])
 lp.a_matrix_.value_ = np.array([0.3, 0.7, 0.5, 0.5], dtype=np.double)
 h.passModel(lp)
+
+# Solve
 h.run()
+
+# Print solution
 solution = h.getSolution()
 basis = h.getBasis()
 info = h.getInfo()
@@ -178,10 +191,11 @@ for irow in range(num_row):
     print(irow, solution.row_value[irow],
           h.basisStatusToString(basis.row_status[irow]))
 
+# ~~~
 # Clear so that incumbent model is empty
 h.clear()
+
 # Now define the test-semi-definite0 model (from TestQpSolver.cpp) as a HighsModel instance
-#
 model = highspy.HighsModel()
 model.lp_.model_name_ = "semi-definite"
 model.lp_.num_col_ = 3
@@ -204,6 +218,7 @@ print("test-semi-definite0 as HighsModel")
 h.passModel(model)
 h.run()
 
+# ~~~
 # Clear so that incumbent model is empty
 h.clear()
 num_col = 3
@@ -253,29 +268,38 @@ h.passModel(
 h.run()
 h.writeSolution("", 1)
 
+# ~~~
 # Clear so that incumbent model is empty
 h.clear()
 print("25fv47 as HighsModel")
 
 h.readModel("check/instances/25fv47.mps")
+
 h.presolve()
 presolved_lp = h.getPresolvedLp()
+
 # Create a HiGHS instance to solve the presolved LP
 print('\nCreate Highs instance to solve presolved LP')
 h1 = highspy.Highs()
 h1.passModel(presolved_lp)
+
+# Get and set options
 options = h1.getOptions()
 options.presolve = "off"
 options.solver = "ipm"
+
 h1.passOptions(options)
 h1.writeOptions("")
+
 h1.run()
 solution = h1.getSolution()
 basis = h1.getBasis()
-print(
-    "\nCrossover then postsolve LP using solution and basis from other Highs instance"
-)
+
+print("Crossover, then postsolve using solution and basis from another instance")
+
 h.postsolve(solution, basis)
+
+# Get solution
 info = h.getInfo()
 model_status = h.getModelStatus()
 print("Model status = ", h.modelStatusToString(model_status))
