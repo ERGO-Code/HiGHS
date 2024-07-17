d318f6500fcc3dc800b142e95275e2d06d7e3087
fwesselm <fwesselm@mathworks.com>
2024-07-17 23:34:49 +0200

    Add comment

--- src/mip/HighsImplications.h
+++ src/mip/HighsImplications.h
@@ -109,6 +109,7 @@ class HighsImplications {
               double vlbconstant);
 
   void columnTransformed(HighsInt col, double scale, double constant) {
+    // Update variable bounds affected by transformation
     if (scale < 0) std::swap(vubs[col], vlbs[col]);
 
     auto transformVbd = [&](HighsInt, VarBound& vbd) {
@@ -120,6 +121,7 @@ class HighsImplications {
     vlbs[col].for_each(transformVbd);
     vubs[col].for_each(transformVbd);
 
+    // Update substitutions affected by transformation
     for (auto& substitution : substitutions) {
       if (substitution.substcol == col) {
         substitution.offset -= constant;
