bc3b5676d4c6e0dbd3ece89c331e481624abc021
Ivet Galabova <galabovaa@gmail.com>
2024-10-16 17:42:31 +0300

    formatting

--- src/lp_data/HighsSolve.cpp
+++ src/lp_data/HighsSolve.cpp
@@ -72,10 +72,10 @@ HighsStatus solveLp(HighsLpSolverObject& solver_object, const string message) {
                                           return_status, "solveLpCupdlp");
     }
     if (return_status == HighsStatus::kError) return return_status;
-      // if (solver_object.model_status_ == HighsModelStatus::kOptimal)
-      // IPM (and PDLP?) can claim optimality with large primal and/or
-      // dual residual errors, so correct any residual errors that
-      // exceed the tolerance
+    // if (solver_object.model_status_ == HighsModelStatus::kOptimal)
+    // IPM (and PDLP?) can claim optimality with large primal and/or
+    // dual residual errors, so correct any residual errors that
+    // exceed the tolerance
     correctResiduals(solver_object);
 
     // Non-error return requires a primal solution
