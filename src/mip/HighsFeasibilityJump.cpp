dc3684b3c311959e8941d15a975c68822ff324e8
JAJHall <jajhall@ed.ac.uk>
2024-11-04 18:56:28 +0000

    Added src/mip/feasibilityjump.hh to BUILD.bazel

--- src/mip/HighsFeasibilityJump.cpp
+++ src/mip/HighsFeasibilityJump.cpp
@@ -13,4 +13,16 @@
 #include "mip/HighsMipSolverData.h"
 #include "mip/feasibilityjump.hh"
 
-void HighsMipSolverData::feasibilityJump() {}
+void HighsMipSolverData::feasibilityJump() {
+
+  const HighsLp* model = this->mipsolver.model_;
+  const bool found_integer_feasible_solution = false;
+  std::vector<double> col_value;
+  double objective_function_value;
+  if (found_integer_feasible_solution) {
+    col_value.assign(model->num_col_, 0);
+    objective_function_value = 0.0;
+    addIncumbent(col_value, objective_function_value,
+		 kSolutionSourceFeasibilityJump);
+  }
+}
