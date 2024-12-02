c4824b1bcf5fbc70dc7a9717015c77102a89c4eb
Ben Champion <6761956+BenChampion@users.noreply.github.com>
2024-12-02 18:25:15 +0000

    Fix uninitialized member in feasibilityjump.hh; code cleanup

--- src/mip/HighsFeasibilityJump.cpp
+++ src/mip/HighsFeasibilityJump.cpp
@@ -10,8 +10,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #include <climits>
 
-#include "lp_data/HighsModelUtils.h" // for typeToString Remove 1423
-
 #include "mip/HighsMipSolverData.h"
 #include "mip/feasibilityjump.hh"
 
@@ -19,17 +17,17 @@ void HighsMipSolverData::feasibilityJump() {
   // This is the (presolved) model being solved
   const HighsLp* model = this->mipsolver.model_;
   const HighsLogOptions& log_options = mipsolver.options_mip_->log_options;
-  printf("HighsMipSolverData::feasibilityJump called with primal bound of %g\n",
-         lower_bound);
+  highsLogUser(
+      log_options, HighsLogType::kInfo,
+      "HighsMipSolverData::feasibilityJump called with primal bound of %g\n",
+      lower_bound);
 
-  const HighsInt MAX_TOTAL_EFFORT = 1e6;
-  const HighsInt MAX_EFFORT_SINCE_LAST_IMPROVEMENT = 1e3;
+  const HighsInt kMaxTotalEffort = 1e6;
+  const HighsInt kMaxEffortSinceLastImprovement = 1e3;
 
   bool found_integer_feasible_solution = false;
   std::vector<double> col_value(model->num_col_, 0.0);
   double objective_function_value;
-
-  printf("DEBUG: configuring feasibility jump\n");
 
   // Configure Feasibility Jump and pass it the problem
   external_feasibilityjump::equalityTolerance = epsilon;
@@ -37,72 +35,73 @@ void HighsMipSolverData::feasibilityJump() {
 
   auto solver = external_feasibilityjump::FeasibilityJumpSolver();
 
-  printf("DEBUG: adding cols (vars)\n");
-
   for (int i = 0; i < model->num_col_; ++i) {
+    assert(model->integrality_[i] == HighsVarType::kContinuous ||
+           model->integrality_[i] == HighsVarType::kInteger ||
+           model->integrality_[i] == HighsVarType::kImplicitInteger);
     external_feasibilityjump::VarType fjVarType;
     if (model->integrality_[i] == HighsVarType::kContinuous) {
       fjVarType = external_feasibilityjump::VarType::Continuous;
-    } else if (model->integrality_[i] == HighsVarType::kInteger) {
-      fjVarType = external_feasibilityjump::VarType::Integer;
     } else {
-      printf(
-          "Feasibility Jump only supports continuous and integer variables, but integrality_[%d] is %s. "
-          "Skipping Feasibility Jump...\n", int(i), typeToString(model->integrality_[i]).c_str());
-      return;
+      fjVarType = external_feasibilityjump::VarType::Integer;
     }
-    solver.addVar(
-      fjVarType,
-      model->col_lower_[i],
-      model->col_upper_[i],
-      model->col_cost_[i]
-    );
+    solver.addVar(fjVarType, model->col_lower_[i], model->col_upper_[i],
+                  model->col_cost_[i]);
   }
 
-  printf("DEBUG: adding rows (constraints)\n");
-
-  HighsInt numNzThisRow;
-  HighsInt* rowIndexBuffer = new HighsInt[model->num_col_];
-  double* rowValueBuffer = new double[model->num_col_];
+  HighsInt row_num_nz;
+  HighsInt* row_index_buffer = new HighsInt[model->num_col_];
+  double* row_value_buffer = new double[model->num_col_];
 
   for (int i = 0; i < model->num_row_; ++i) {
     bool hasFiniteLower = std::isfinite(model->row_lower_[i]);
     bool hasFiniteUpper = std::isfinite(model->row_upper_[i]);
     if (hasFiniteLower || hasFiniteUpper) {
-      model->a_matrix_.getRow(i, numNzThisRow, rowIndexBuffer, rowValueBuffer);
+      model->a_matrix_.getRow(i, row_num_nz, row_index_buffer,
+                              row_value_buffer);
       // TODO: check if relax_continuous == 1 really is the right thing to pass
       if (hasFiniteLower) {
         solver.addConstraint(external_feasibilityjump::RowType::Gte,
-                             model->row_lower_[i], numNzThisRow, rowIndexBuffer,
-                             rowValueBuffer, 1);
+                             model->row_lower_[i], row_num_nz, row_index_buffer,
+                             row_value_buffer, 1);
       }
       if (hasFiniteUpper) {
         solver.addConstraint(external_feasibilityjump::RowType::Lte,
-                             model->row_upper_[i], numNzThisRow, rowIndexBuffer,
-                             rowValueBuffer, 1);
+                             model->row_upper_[i], row_num_nz, row_index_buffer,
+                             row_value_buffer, 1);
       }
-
     }
   }
 
-  delete[] rowValueBuffer;
-  delete[] rowIndexBuffer;
-
-  printf("DEBUG: calling Feasibility Jump\n");
+  delete[] row_value_buffer;
+  delete[] row_index_buffer;
 
   auto fjControlCallback =
       [=, &col_value, &found_integer_feasible_solution,
        &objective_function_value](external_feasibilityjump::FJStatus status)
       -> external_feasibilityjump::CallbackControlFlow {
-    highsLogUser(log_options, HighsLogType::kInfo, "From Feasibility Jump callback\n");
+    highsLogUser(log_options, HighsLogType::kInfo,
+                 "From Feasibility Jump callback\n");
+    highsLogUser(log_options, HighsLogType::kInfo, "Total effort: %d\n",
+                 status.totalEffort);
+    highsLogUser(log_options, HighsLogType::kInfo,
+                 "Effort since last improvement: %d\n",
+                 status.effortSinceLastImprovement);
+    highsLogUser(log_options, HighsLogType::kInfo, "Number of variables: %d\n",
+                 status.numVars);
+    highsLogUser(log_options, HighsLogType::kInfo,
+                 "Solution objective value: %0.2f\n",
+                 status.solutionObjectiveValue);
+    highsLogUser(log_options, HighsLogType::kInfo, "Solution found: %d\n",
+                 status.solution != nullptr);
     if (status.solution != nullptr) {
       found_integer_feasible_solution = true;
       col_value = std::vector<double>(status.solution,
                                       status.solution + status.numVars);
       objective_function_value = status.solutionObjectiveValue;
     }
-    if (status.effortSinceLastImprovement > MAX_EFFORT_SINCE_LAST_IMPROVEMENT ||
-        status.totalEffort > MAX_TOTAL_EFFORT) {
+    if (status.effortSinceLastImprovement > kMaxEffortSinceLastImprovement ||
+        status.totalEffort > kMaxTotalEffort) {
       return external_feasibilityjump::CallbackControlFlow::Terminate;
     } else {
       return external_feasibilityjump::CallbackControlFlow::Continue;
@@ -114,11 +113,15 @@ void HighsMipSolverData::feasibilityJump() {
   if (found_integer_feasible_solution) {
     // Feasibility jump has found a solution, so call addIncumbent to
     // (possibly) update the incumbent
-    highsLogUser(log_options, HighsLogType::kInfo, "Feasibility Jump has found an integer feasible solution with objective value %g\n",
-		 objective_function_value);
+    highsLogUser(log_options, HighsLogType::kInfo,
+                 "Feasibility Jump has found an integer feasible solution with "
+                 "objective value %g\n",
+                 objective_function_value);
     addIncumbent(col_value, objective_function_value,
                  kSolutionSourceFeasibilityJump);
   } else {
-    highsLogUser(log_options, HighsLogType::kInfo, "Feasibility Jump has not found an integer feasible solution\n");
+    highsLogUser(
+        log_options, HighsLogType::kInfo,
+        "Feasibility Jump has not found an integer feasible solution\n");
   }
 }
