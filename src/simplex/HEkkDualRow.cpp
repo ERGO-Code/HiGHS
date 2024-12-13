5b295cc9a6c465fbba728a460c4a6a367fac665d
fwesselm <fwesselm@mathworks.com>
2024-12-13 10:00:08 +0100

    Use const reference

--- src/simplex/HEkkDualRow.cpp
+++ src/simplex/HEkkDualRow.cpp
@@ -587,7 +587,7 @@ void HEkkDualRow::createFreemove(HVector* row_ep) {
                 : ekk_instance_.info_.update_count < 20 ? 3e-8
                                                         : 1e-6;
     HighsInt move_out = workDelta < 0 ? -1 : 1;
-    for (HighsInt iVar : freeList) {
+    for (const HighsInt& iVar : freeList) {
       assert(iVar < ekk_instance_.lp_.num_col_ + ekk_instance_.lp_.num_row_);
       double alpha = ekk_instance_.lp_.a_matrix_.computeDot(*row_ep, iVar);
       if (fabs(alpha) > Ta) {
@@ -601,7 +601,7 @@ void HEkkDualRow::createFreemove(HVector* row_ep) {
 }
 void HEkkDualRow::deleteFreemove() {
   if (!freeList.empty()) {
-    for (HighsInt iVar : freeList) {
+    for (const HighsInt& iVar : freeList) {
       assert(iVar < ekk_instance_.lp_.num_col_ + ekk_instance_.lp_.num_row_);
       ekk_instance_.basis_.nonbasicMove_[iVar] = 0;
     }
