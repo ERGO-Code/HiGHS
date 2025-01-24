51ac7859173f68cc0a6279c9008810496cb2df78
JAJHall <jajhall@ed.ac.uk>
2025-01-24 14:55:33 +0000

    Formatted

--- src/lp_data/HighsOptions.h
+++ src/lp_data/HighsOptions.h
@@ -851,7 +851,7 @@ class HighsOptions : public HighsOptionsStruct {
 
     record_bool = new OptionRecordBool(
         "timeless_log", "Suppression of time-based data in logging", true,
-        &timeless_log, false);// true); //
+        &timeless_log, false);  // true); //
     records.push_back(record_bool);
 
     record_string =
