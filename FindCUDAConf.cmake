f54c98f5d18b1804544c49212dbe0b80a96950cd
Ivet Galabova <galabovaa@gmail.com>
2025-01-31 14:58:50 +0000

    wip:
    "

--- FindCUDAConf.cmake
+++ FindCUDAConf.cmake
@@ -6,28 +6,39 @@ set(CMAKE_CUDA_ARCHITECTURES "all")
 set(CMAKE_CUDA_PATH "${CUDA_HOME}")
 set(CMAKE_CUDA_COMPILER "${CMAKE_CUDA_PATH}/bin/nvcc")
 
+# include(CheckLanguage)
+# check_language(CUDA)
+
 enable_language(CUDA)
+# set(CUDA_HOME "C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v12.6")
+# find_package(CUDAToolkit HINTS "C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v12.6")
 
 find_library(CUDA_LIBRARY_ART
         NAMES cudart
         HINTS "${CMAKE_CUDA_PATH}/lib64/"
+        HINTS "${CMAKE_CUDA_PATH}/lib/x64/"
         REQUIRED
 )
 find_library(CUDA_LIBRARY_SPS
         NAMES cusparse
         HINTS "${CMAKE_CUDA_PATH}/lib64/"
+        HINTS "${CMAKE_CUDA_PATH}/lib/x64/"
         REQUIRED
 )
 find_library(CUDA_LIBRARY_BLS
         NAMES cublas
         HINTS "${CMAKE_CUDA_PATH}/lib64/"
+        HINTS "${CMAKE_CUDA_PATH}/lib/x64/"
         REQUIRED
 )
+
 if (${CUDA_LIBRARY-NOTFOUND})
-    message(WARNING "    - CUDA Libraries not detected at ${CUDA_HOME}")
+    message(
+        ARNING "    - CUDA Libraries not detected at ${CUDA_HOME}")
 else ()
     message(NOTICE "    - CUDA Libraries detected at ${CUDA_HOME}")
-    set(CUDA_LIBRARY ${CUDA_LIBRARY_ART} ${CUDA_LIBRARY_SPS} ${CUDA_LIBRARY_BLS})
+    set(CUDA_LIBRARY ${CUDA_LIBRARY_ART} ${CUDA_LIBRARY_SPS} ${CUDCUDA_HOMEA_LIBRARY_BLS})
+    # set(CUDA_LIBRARY CUDA::cudart CUDA::cusparse CUDA::cublas)
     message(NOTICE "    -   :${CUDA_LIBRARY}")
 endif ()
 
