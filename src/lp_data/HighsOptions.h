c88d8db9f6e068addb5aa219ac877d6a8d57af24
Derya Ozyurt <dozyurt@mathworks.com>
2025-02-18 23:29:06 +0000

    Turned Shifting off by default to see if tests still fail.

--- src/lp_data/HighsOptions.h
+++ src/lp_data/HighsOptions.h
@@ -1118,7 +1118,7 @@ class HighsOptions : public HighsOptionsStruct {
 
     record_bool = new OptionRecordBool(
         "mip_heuristic_run_Shifting",
-        "Run Shifting heuristic: Default = true", advanced,
+        "Run Shifting heuristic: Default = false", advanced,
         &mip_heuristic_run_Shifting, true);
     records.push_back(record_bool);
 
