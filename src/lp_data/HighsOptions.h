11c883422c83159d595091c663977fcf09e25455
Derya Ozyurt <dozyurt@mathworks.com>
2025-02-19 02:31:25 +0000

    Also turning ZI Round off.

--- src/lp_data/HighsOptions.h
+++ src/lp_data/HighsOptions.h
@@ -1113,7 +1113,7 @@ class HighsOptions : public HighsOptionsStruct {
     record_bool = new OptionRecordBool(
         "mip_heuristic_run_ZIRound",
         "Run ZI Round heuristic: Default = true", advanced,
-        &mip_heuristic_run_ZIRound, true);
+        &mip_heuristic_run_ZIRound, false);
     records.push_back(record_bool);
 
     record_bool = new OptionRecordBool(
