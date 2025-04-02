c87b9deba30ae3fe1535142bb7382081e782953d
Ivet Galabova <galabovaa@gmail.com>
2025-04-02 14:27:56 +0300

    try install with vscode integration

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -15,7 +15,7 @@ IF "%VS_VER%"=="2019_build_tools" (
 )
 
 IF "%VS_VER%"=="2022_build_tools" (
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 )
 
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
--- .github/install_windows.bat
+++ .github/install_windows.bat
@@ -8,4 +8,4 @@ curl.exe --output webimage.exe --url %URL% --retry 5 --retry-delay 5
 start /b /wait webimage.exe -s -x -f webimage_extracted --log extract.log
 del webimage.exe
 
-webimage_extracted\bootstrapper.exe -s --action install --eula=accept -p=NEED_VS2022_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 --log-dir=.
+webimage_extracted\bootstrapper.exe -s --action install --eula=accept --log-dir=.
