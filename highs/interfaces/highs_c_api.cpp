11b9f92b58215639888167fda26f4d4aaaeb51e7
Luke Marshall <52978038+mathgeekcoder@users.noreply.github.com>
2025-04-14 17:25:38 -0700

    Fixed formatting issue and fixed HighsInt/int build issue.

--- highs/interfaces/highs_c_api.cpp
+++ highs/interfaces/highs_c_api.cpp
@@ -1493,7 +1493,7 @@ const void* Highs_getCallbackDataOutItem(const HighsCallbackDataOut* data_out,
   return nullptr;
 }
 
-int Highs_setCallbackSolution(HighsCallbackDataIn* data_in,
+HighsInt Highs_setCallbackSolution(HighsCallbackDataIn* data_in,
                               HighsInt num_entries, const double* value) {
   if (data_in != nullptr && data_in->cbdata != nullptr) {
     HighsCallbackInput* obj = static_cast<HighsCallbackInput*>(data_in->cbdata);
@@ -1502,7 +1502,7 @@ int Highs_setCallbackSolution(HighsCallbackDataIn* data_in,
     return static_cast<int>(HighsStatus::kError);
 }
 
-int Highs_setCallbackSparseSolution(HighsCallbackDataIn* data_in,
+HighsInt Highs_setCallbackSparseSolution(HighsCallbackDataIn* data_in,
                                      HighsInt num_entries,
                                      const HighsInt* index,
                                      const double* value) {
@@ -1513,7 +1513,7 @@ int Highs_setCallbackSparseSolution(HighsCallbackDataIn* data_in,
     return static_cast<int>(HighsStatus::kError);
 }
 
-int Highs_repairCallbackSolution(HighsCallbackDataIn* data_in) {
+HighsInt Highs_repairCallbackSolution(HighsCallbackDataIn* data_in) {
   if (data_in != nullptr && data_in->cbdata != nullptr) {
     HighsCallbackInput* obj = static_cast<HighsCallbackInput*>(data_in->cbdata);
     return static_cast<int>(obj->repairSolution());
