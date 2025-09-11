848ced17858de95457fffffcd0417b74c3d1cf62
Mark Turner <turner@zib.de>
2025-09-11 12:16:01 +0200

    Add thread safe undoPrimal option

--- highs/presolve/HighsPostsolveStack.h
+++ highs/presolve/HighsPostsolveStack.h
@@ -609,8 +609,22 @@ class HighsPostsolveStack {
 
   /// undo presolve steps for primal dual solution and basis
   void undo(const HighsOptions& options, HighsSolution& solution,
-            HighsBasis& basis, const HighsInt report_col = -1) {
-    reductionValues.resetPosition();
+            HighsBasis& basis, const HighsInt report_col = -1,
+            const bool thread_safe = false) {
+    HighsDataStack reductionValuesCopy;
+    std::vector<Nonzero> colValuesCopy;
+    std::vector<Nonzero> rowValuesCopy;
+    if (!thread_safe) {
+      reductionValues.resetPosition();
+    } else {
+      reductionValuesCopy = reductionValues;
+      reductionValuesCopy.resetPosition();
+      colValuesCopy = colValues;
+      rowValuesCopy = rowValues;
+    }
+    HighsDataStack& reductionValues_ = thread_safe ? reductionValuesCopy : reductionValues;
+    std::vector<Nonzero>& colValues_ = thread_safe ? colValuesCopy : colValues;
+    std::vector<Nonzero>& rowValues_ = thread_safe ? rowValuesCopy : rowValues;
 
     // Verify that undo can be performed
     assert(solution.value_valid);
@@ -648,97 +662,97 @@ class HighsPostsolveStack {
       switch (reductions[i - 1].first) {
         case ReductionType::kLinearTransform: {
           LinearTransform reduction;
-          reductionValues.pop(reduction);
+          reductionValues_.pop(reduction);
           reduction.undo(options, solution);
           break;
         }
         case ReductionType::kFreeColSubstitution: {
           FreeColSubstitution reduction;
-          reductionValues.pop(colValues);
-          reductionValues.pop(rowValues);
-          reductionValues.pop(reduction);
-          reduction.undo(options, rowValues, colValues, solution, basis);
+          reductionValues_.pop(colValues_);
+          reductionValues_.pop(rowValues_);
+          reductionValues_.pop(reduction);
+          reduction.undo(options, rowValues_, colValues_, solution, basis);
           break;
         }
         case ReductionType::kDoubletonEquation: {
           DoubletonEquation reduction;
-          reductionValues.pop(colValues);
-          reductionValues.pop(reduction);
-          reduction.undo(options, colValues, solution, basis);
+          reductionValues_.pop(colValues_);
+          reductionValues_.pop(reduction);
+          reduction.undo(options, colValues_, solution, basis);
           break;
         }
         case ReductionType::kEqualityRowAddition: {
           EqualityRowAddition reduction;
-          reductionValues.pop(rowValues);
-          reductionValues.pop(reduction);
-          reduction.undo(options, rowValues, solution, basis);
+          reductionValues_.pop(rowValues_);
+          reductionValues_.pop(reduction);
+          reduction.undo(options, rowValues_, solution, basis);
           break;
         }
         case ReductionType::kEqualityRowAdditions: {
           EqualityRowAdditions reduction;
-          reductionValues.pop(colValues);
-          reductionValues.pop(rowValues);
-          reductionValues.pop(reduction);
-          reduction.undo(options, rowValues, colValues, solution, basis);
+          reductionValues_.pop(colValues_);
+          reductionValues_.pop(rowValues_);
+          reductionValues_.pop(reduction);
+          reduction.undo(options, rowValues_, colValues_, solution, basis);
           break;
         }
         case ReductionType::kSingletonRow: {
           SingletonRow reduction;
-          reductionValues.pop(reduction);
+          reductionValues_.pop(reduction);
           reduction.undo(options, solution, basis);
           break;
         }
         case ReductionType::kFixedCol: {
           FixedCol reduction;
-          reductionValues.pop(colValues);
-          reductionValues.pop(reduction);
-          reduction.undo(options, colValues, solution, basis);
+          reductionValues_.pop(colValues_);
+          reductionValues_.pop(reduction);
+          reduction.undo(options, colValues_, solution, basis);
           break;
         }
         case ReductionType::kRedundantRow: {
           RedundantRow reduction;
-          reductionValues.pop(reduction);
+          reductionValues_.pop(reduction);
           reduction.undo(options, solution, basis);
           break;
         }
         case ReductionType::kForcingRow: {
           ForcingRow reduction;
-          reductionValues.pop(rowValues);
-          reductionValues.pop(reduction);
-          reduction.undo(options, rowValues, solution, basis);
+          reductionValues_.pop(rowValues_);
+          reductionValues_.pop(reduction);
+          reduction.undo(options, rowValues_, solution, basis);
           break;
         }
         case ReductionType::kForcingColumn: {
           ForcingColumn reduction;
-          reductionValues.pop(colValues);
-          reductionValues.pop(reduction);
-          reduction.undo(options, colValues, solution, basis);
+          reductionValues_.pop(colValues_);
+          reductionValues_.pop(reduction);
+          reduction.undo(options, colValues_, solution, basis);
           break;
         }
         case ReductionType::kForcingColumnRemovedRow: {
           ForcingColumnRemovedRow reduction;
-          reductionValues.pop(rowValues);
-          reductionValues.pop(reduction);
-          reduction.undo(options, rowValues, solution, basis);
+          reductionValues_.pop(rowValues_);
+          reductionValues_.pop(reduction);
+          reduction.undo(options, rowValues_, solution, basis);
           break;
         }
         case ReductionType::kDuplicateRow: {
           DuplicateRow reduction;
-          reductionValues.pop(reduction);
+          reductionValues_.pop(reduction);
           reduction.undo(options, solution, basis);
           break;
         }
         case ReductionType::kDuplicateColumn: {
           DuplicateColumn reduction;
-          reductionValues.pop(reduction);
+          reductionValues_.pop(reduction);
           reduction.undo(options, solution, basis);
           break;
         }
         case ReductionType::kSlackColSubstitution: {
           SlackColSubstitution reduction;
-          reductionValues.pop(rowValues);
-          reductionValues.pop(reduction);
-          reduction.undo(options, rowValues, solution, basis);
+          reductionValues_.pop(rowValues_);
+          reductionValues_.pop(reduction);
+          reduction.undo(options, rowValues_, solution, basis);
           break;
         }
         default:
@@ -763,14 +777,15 @@ class HighsPostsolveStack {
 
   /// undo presolve steps for primal solution
   void undoPrimal(const HighsOptions& options, HighsSolution& solution,
-                  const HighsInt report_col = -1) {
+                  const HighsInt report_col = -1,
+                  const bool thread_safe = false) {
     // Call to reductionValues.resetPosition(); seems unnecessary as
     // it's the first thing done in undo
-    reductionValues.resetPosition();
+    if (!thread_safe) reductionValues.resetPosition();
     HighsBasis basis;
     basis.valid = false;
     solution.dual_valid = false;
-    undo(options, solution, basis, report_col);
+    undo(options, solution, basis, report_col, thread_safe);
   }
 
   /*
