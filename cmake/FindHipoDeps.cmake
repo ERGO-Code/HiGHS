e37e8c5af0a0a89a28647fb22705dad811ab571c
Ivet Galabova <galabovaa@gmail.com>
2025-10-13 17:12:41 +0100

    works on windows locally with patched up metis

--- cmake/FindHipoDeps.cmake
+++ cmake/FindHipoDeps.cmake
@@ -1,73 +1,34 @@
 # BLAS
-option(BLAS_ROOT "Root directory of BLAS or OpenBLAS" "")
-message(STATUS "BLAS_ROOT is " ${BLAS_ROOT})
 
-if (WIN32)
+find_package(BLAS REQUIRED)
 
-    find_package(OpenBLAS CONFIG REQUIRED) 
-    message(STATUS "OpenBLAS CMake config path: ${OpenBLAS_DIR}")
-
-    find_package(metis CONFIG REQUIRED)
-    message(STATUS "metis CMake config path: ${metis_DIR}")
-
-    # find_package(gklib CONFIG REQUIRED)
-    # message(STATUS "gklib CMake config path: ${gklib_DIR}")
-elseif(NOT APPLE)
-    # LINUX
-    find_library(OPENBLAS_LIB 
-        NAMES openblas 
-        HINTS "${BLAS_ROOT}/lib")
-
-    if(OPENBLAS_LIB)
-        message("Found OpenBLAS library at ${OPENBLAS_LIB}")
-
-    else(OPENBLAS_LIB)
-        find_library(BLAS_LIB 
-            NAMES blas HINTS 
-            "${BLAS_ROOT}/lib")
-
-        if(NOT BLAS_LIB)
-            message(FATAL_ERROR "No BLAS library found")
-        endif(NOT BLAS_LIB)
-        message("Found BLAS library at ${BLAS_LIB}")
-    endif(OPENBLAS_LIB)
-
+if(BLAS_FOUND)
+    message(STATUS "Using BLAS library: ${BLAS_LIBRARIES}")
+    message(STATUS "BLAS include dirs: ${BLAS_INCLUDE_DIRS}")
 endif()
 
-if (NOT WIN32)
-    # Find_package works if deps are installed with vcpkg.
+# METIS
+option(METIS_ROOT "Root directory of METIS" "")
+message(STATUS "METIS_ROOT is " ${METIS_ROOT})
 
-    # METIS
-    option(METIS_ROOT "Root directory of METIS" "")
-    message(STATUS "METIS_ROOT is " ${METIS_ROOT})
+find_package(metis CONFIG)
 
-    find_path(METIS_PATH 
-        NAMES "metis.h" 
+if(metis_FOUND)
+    message(STATUS "metis CMake config path: ${metis_DIR}")
+else()
+    find_path(METIS_PATH
+        NAMES "metis.h"
         REQUIRED
-        HINTS "${METIS_ROOT}/include")
+        PATHS "${METIS_ROOT}/include"
+        NO_DEFAULT_PATH)
 
     message("Found Metis header at ${METIS_PATH}")
 
-    find_library(METIS_LIB 
-        NAMES metis 
+    find_library(METIS_LIB
+        NAMES metis libmetis
         REQUIRED
-        HINTS "${METIS_ROOT}/lib")
+        PATHS "${METIS_ROOT}/lib" "${METIS_ROOT}/bin"
+        NO_DEFAULT_PATH)
+
     message("Found Metis library at ${METIS_LIB}")
-
-    # GKlib
-    option(GKLIB_ROOT "Root directory of GKlib" "")
-    message(STATUS "GKLIB_ROOT is " ${GKLIB_ROOT})
-
-    find_path(GKLIB_PATH 
-        NAMES "GKlib.h" REQUIRED
-        HINTS "${GKLIB_ROOT}/include")
-
-    message("Found GKlib header at ${GKLIB_PATH}")
-
-    find_library(GKLIB_LIB 
-        NAMES GKlib
-        REQUIRED
-        HINTS "${GKLIB_ROOT}/lib")
-
-    message("Found GKlib library at ${GKLIB_LIB}")
 endif()
