66137f381e94ee0123bfd47a2bcff103d2773afd
Ivet Galabova <galabovaa@gmail.com>
2025-10-14 12:45:53 +0100

    latest metis working locally on macos

--- cmake/FindHipoDeps.cmake
+++ cmake/FindHipoDeps.cmake
@@ -1,12 +1,10 @@
 # BLAS
-option(BLAS_ROOT "Root directory of BLAS or OpenBLAS" "")
+set(BLAS_ROOT "" CACHE STRING "Root directory of BLAS or OpenBLAS")
 message(STATUS "BLAS_ROOT is " ${BLAS_ROOT})
 
 if (WIN32)
-
     find_package(OpenBLAS CONFIG REQUIRED) 
     message(STATUS "OpenBLAS CMake config path: ${OpenBLAS_DIR}")
-
 elseif(NOT APPLE)
     # LINUX
     find_library(OPENBLAS_LIB 
@@ -26,11 +24,10 @@ elseif(NOT APPLE)
         endif(NOT BLAS_LIB)
         message("Found BLAS library at ${BLAS_LIB}")
     endif(OPENBLAS_LIB)
-
 endif()
 
 # METIS
-option(METIS_ROOT "Root directory of METIS" "")
+set(METIS_ROOT "" CACHE STRING "Root directory of METIS")
 message(STATUS "METIS_ROOT is " ${METIS_ROOT})
 
 find_package(metis CONFIG)
@@ -53,4 +50,47 @@ else()
         NO_DEFAULT_PATH)
 
     message("Found Metis library at ${METIS_LIB}")
+endif()
+
+# GKlib optional for newer versions on ubuntu and macos
+set(GKLIB_ROOT "" CACHE STRING "Root directory of GKlib")
+if (NOT (GKLIB_ROOT STREQUAL ""))
+    message(STATUS "GKLIB_ROOT is " ${GKLIB_ROOT})
+
+    find_package(GKlib CONFIG)
+
+    if(GKlib_FOUND)
+        message(STATUS "gklib CMake config path: ${GKlib_DIR}")
+
+        # get_cmake_property(_vars VARIABLES)
+        # foreach(_v IN LISTS _vars)
+        #     if(_v MATCHES "GKlib")
+        #     message(STATUS "${_v} = ${${_v}}")
+        #     endif()
+        # endforeach()
+
+        # get_property(_targets DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY IMPORTED_TARGETS)
+        # foreach(_t IN LISTS _targets)
+        # if(_t MATCHES "GKlib")
+        #     message(STATUS "GKlib exported target: ${_t}")
+        # endif()
+        # endforeach()
+
+    else()
+        find_path(GKLIB_PATH
+            NAMES "gklib.h"
+            REQUIRED
+            PATHS "${GKLIB_ROOT}/include"
+            NO_DEFAULT_PATH)
+
+        message("Found GKlib header at ${GKLIB_PATH}")
+
+        find_library(GKLIB_LIB
+            NAMES GKlib libGKlib
+            REQUIRED
+            PATHS "${GKLIB_ROOT}/lib"
+            NO_DEFAULT_PATH)
+
+        message("Found GKlib library at ${GKLIB_LIB}")
+    endif()
 endif()
