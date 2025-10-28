3728494f30580c0717b6776ce955697589863320
Stefan Vigerske <svigerske@gams.com>
2025-10-28 10:08:33 +0100

    bring back comment line that was probably removed by accident

--- highs/ipm/IpxWrapper.cpp
+++ highs/ipm/IpxWrapper.cpp
@@ -1,3 +1,4 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /*                                                                       */
 /*    This file is part of the HiGHS linear optimization suite           */
 /*                                                                       */
