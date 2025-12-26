f972839da51841fbaf8774545dde0cc9d360539f
Thiago Novaes <77932135+Thiago-NovaesB@users.noreply.github.com>
2025-12-26 15:19:53 +0100

    test

---
+++ src/interfaces/HighsInterface/HighsInterface/Enums/ObjectiveSense.cs
@@ -0,0 +1,16 @@
+﻿namespace Highs.Enums;
+
+/// <summary>
+/// This defines optimization sense of a HighsLp
+/// </summary>
+public enum ObjectiveSense
+{
+    /// <summary>
+    /// The objective is to be minimized
+    /// </summary>
+    Minimize = 1,
+    /// <summary>
+    /// The objective is to be maximized
+    /// </summary>
+    Maximize = -1
+}
---
+++ src/interfaces/HighsInterface/HighsInterface/Records/SolutionInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace Highs;
+
+/// <summary>
+/// The solution info.
+/// </summary>
+/// <param name="SimplexIterationCount">The simplex iteration count.</param>
+/// <param name="IpmIterationCount">The Interior Point Method (IPM) iteration count.</param>
+/// <param name="PdlpIterationCount">The PDLP iteration count.</param>
+/// <param name="MipGap">The MIP gap.</param>
+/// <param name="DualBound">The best dual bound.</param>
+/// <param name="NodeCount">The MIP node count.</param>
+/// <param name="ObjectiveValue">The objective value.</param>
+public record SolutionInfo(int SimplexIterationCount,
+                           int IpmIterationCount,
+                           int PdlpIterationCount,
+                           double MipGap,
+                           double DualBound,
+                           long NodeCount,
+                           double ObjectiveValue)
+{
+}
