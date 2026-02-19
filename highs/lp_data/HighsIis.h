7b294ba7afdbb4b1d73fea9cf0a2120654eabed6
hbagheri <hbagheri@mathworks.com>
2026-02-19 13:50:34 -0500

    Removed extra qualification from HighsIis.h

--- highs/lp_data/HighsIis.h
+++ highs/lp_data/HighsIis.h
@@ -85,12 +85,11 @@ class HighsIis {
 
   HighsStatus compute(const HighsLp& lp, const HighsOptions& options,
                       const HighsBasis* basis = nullptr);
-  void HighsIis::processBoundRelaxation(Highs& highs, const bool row_deletion,
-                                        const bool drop_lower,
-                                        const HighsInt iX, double& lower,
-                                        double& upper,
-                                        IisModelStatus& iis_status,
-                                        HighsStatus& search_return_status);
+  void processBoundRelaxation(Highs& highs, const bool row_deletion,
+                              const bool drop_lower, const HighsInt iX,
+                              double& lower, double& upper,
+                              IisModelStatus& iis_status,
+                              HighsStatus& search_return_status);
 
   bool trivial(const HighsLp& lp, const HighsOptions& options);
   bool rowValueBounds(const HighsLp& lp, const HighsOptions& options);
