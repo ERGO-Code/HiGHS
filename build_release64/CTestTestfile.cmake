f07dac5e48bf58ccf7227fc23d26878237653867
Ivet Galabova <galabovaa@gmail.com>
2026-03-12 14:12:00 +0000

    cleared build dir and disable highsint64 by default

--- build_release64/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yzhou/Github/HiGHS
-# Build directory: /home/yzhou/Github/HiGHS/build_release64
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("highs")
-subdirs("app")
-subdirs("check")
