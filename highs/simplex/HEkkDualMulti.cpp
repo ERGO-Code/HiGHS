e940f73c5faf81251de29480c2ff4162ca740800
JAJHall <jajhall@ed.ac.uk>
2026-05-08 18:50:35 +0100

    Fixed errors exposed by AI agent that do not lead to incorrect execution

--- highs/simplex/HEkkDualMulti.cpp
+++ highs/simplex/HEkkDualMulti.cpp
@@ -843,9 +843,9 @@ void HEkkDual::majorUpdatePrimal() {
         // Update steepest edge weights
         HVector* Row = finish->row_ep;
         double Kai = -2 / finish->alpha_row;
-        ekk_instance_.updateDualSteepestEdgeWeights(row_out, variable_in, Col,
-                                                    new_pivotal_edge_weight,
-                                                    Kai, Row->array.data());
+        ekk_instance_.updateDualSteepestEdgeWeights(
+            finish->row_out, finish->variable_in, Col, new_pivotal_edge_weight,
+            Kai, Row->array.data());
       } else if (edge_weight_mode == EdgeWeightMode::kDevex &&
                  !new_devex_framework) {
         // Update Devex weights
