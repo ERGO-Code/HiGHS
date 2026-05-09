c10f425d3493825c90bb3803dc82e3baa305ab62
JAJHall <jajhall@ed.ac.uk>
2026-05-09 12:20:19 +0100

    Correction for PAMI dual simplex

--- highs/simplex/HEkkDualMulti.cpp
+++ highs/simplex/HEkkDualMulti.cpp
@@ -891,9 +891,9 @@ void HEkkDual::majorUpdatePrimal() {
         // Devex
         for (HighsInt jFn = 0; jFn < iFn; jFn++) {
           HighsInt jRow = multi_finish[jFn].row_out;
-          const double aa_iRow = colArray[iRow];
-          edge_weight[jRow] = max(edge_weight[jRow],
-                                  new_pivotal_edge_weight * aa_iRow * aa_iRow);
+          const double value = colArray[jRow];
+          edge_weight[jRow] =
+              max(edge_weight[jRow], new_pivotal_edge_weight * value * value);
         }
         edge_weight[iRow] = new_pivotal_edge_weight;
         num_devex_iterations++;
