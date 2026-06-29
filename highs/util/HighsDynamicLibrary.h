e79dc2151d486085d0577c028900e4e23f943e7f
Ivo Hedtke <ivo.hedtke@dsv.com>
2026-06-29 12:35:59 +0200

    Fix formattiong

--- highs/util/HighsDynamicLibrary.h
+++ highs/util/HighsDynamicLibrary.h
@@ -44,7 +44,8 @@ class HighsDynamicLibrary {
   template <typename FuncType, typename... Args>
 #if (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || \
     (defined(__cplusplus) && __cplusplus >= 201703L)
-  std::invoke_result_t<FuncType, Args...> call(const char* name, Args... args) const {
+  std::invoke_result_t<FuncType, Args...> call(const char* name,
+                                               Args... args) const {
 #else
   typename std::result_of<FuncType(Args...)>::type call(const char* name,
                                                         Args... args) const {
