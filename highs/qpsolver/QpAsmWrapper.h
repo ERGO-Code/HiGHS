fc7b28980b08b3742287677addc5f65e6bb6c609
Julian Hall <jajhall@gmail.com>
2026-08-04 01:26:24 +0100

    Added qpsolver/QpAsmWrapper.h to highs-formatDIR

--- highs/qpsolver/QpAsmWrapper.h
+++ highs/qpsolver/QpAsmWrapper.h
@@ -14,17 +14,15 @@
 #include <algorithm>
 #include <cassert>
 
-#include "lp_data/HighsSolution.h"
 #include "lp_data/HighsQpSolverObject.h"
+#include "lp_data/HighsSolution.h"
 
 HighsStatus solveQpAsm(HighsQpSolverObject& solver_object);
 
 HighsStatus solveQpAsm(const HighsOptions& options, HighsTimer& timer,
                        const HighsLp& lp, const HighsHessian& hessian,
-		       HighsBasis& basis,
-                       HighsSolution& solution,
-                       HighsModelStatus& model_status,
-		       HighsInfo& info,
+                       HighsBasis& basis, HighsSolution& solution,
+                       HighsModelStatus& model_status, HighsInfo& info,
                        HighsCallback& callback);
 
 #endif
