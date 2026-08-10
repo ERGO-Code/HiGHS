4b8bf3d883b69b0b793323b95a9f8a567f88b2fd
fwesselm <fwesselm@mathworks.com>
2026-08-10 12:43:07 +0200

    Merge chaser

--- highs/presolve/HPresolveAnalysis.cpp
+++ highs/presolve/HPresolveAnalysis.cpp
@@ -19,6 +19,8 @@ void HPresolveAnalysis::setup(const HighsLp* model_,
   numDeletedRows = &numDeletedRows_;
   numDeletedCols = &numDeletedCols_;
 
+  this->allow_rule_.assign(kPresolveRuleCount, true);
+
   timer_ = timer;
   const bool lp_presolve = !model_->isMip() || options->solve_relaxation;
   analyse_presolve_time_ =
