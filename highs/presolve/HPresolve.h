98487ec58cbb4601459683c46b0e03f3062ced95
Julian Hall <jajhall@gmail.com>
2026-09-17 11:34:02 +0100

    Moved Leona's unused presolve debug code to highs/presolve/HPresolveDebug.cpp and commented out since it\'s not called

--- highs/presolve/HPresolve.h
+++ highs/presolve/HPresolve.h
@@ -394,10 +394,6 @@ class HPresolve {
   void setInput(HighsMipSolver& mipsolver,
                 const HighsInt presolve_reduction_limit);
 
-  void setReductionLimit(size_t reductionLimit) {
-    this->reductionLimit = reductionLimit;
-  }
-
   bool okSetupPresolveDataStructures();
   void setupSubstitutionOpportunities();
 
@@ -495,10 +491,6 @@ class HPresolve {
 
   HighsModelStatus run(HighsPostsolveStack& postsolve_stack);
 
-  void computeIntermediateMatrix(std::vector<HighsInt>& flagRow,
-                                 std::vector<HighsInt>& flagCol,
-                                 size_t& numreductions);
-
   void substitute(HighsInt substcol, HighsInt staycol, double offset,
                   double scale);
 
@@ -564,8 +556,18 @@ class HPresolve {
       HighsPostsolveStack& postsolve_stack);
   Result presolveRuleTestFourierMotzkin(HighsPostsolveStack& postsolve_stack);
 
-  // Not currently called
+  /*
+  // Methods defined and used in HPresolveDebug, and only executed if
+  // HPresolve::debug is called. This hasn't been used for ages, and
+  // is retained in case it's useful in future
   static void debug(const HighsLp& lp, const HighsOptions& options);
+  void computeIntermediateMatrix(std::vector<HighsInt>& flagRow,
+                                 std::vector<HighsInt>& flagCol,
+                                 size_t& numreductions);
+  void setReductionLimit(size_t reductionLimit) {
+    this->reductionLimit = reductionLimit;
+  }
+  */
 };
 
 }  // namespace presolve
